7e590abd27bb17419aea2fb78841c4c36786c02e
Ceiro Kilp <ceiro.kilp@we.ee>
2022-12-19 14:13:25 +0200

    feat: add Button component

---
+++ GUI/src/components/Button/index.tsx
@@ -0,0 +1,36 @@
+import { ButtonHTMLAttributes, FC, PropsWithChildren, useRef } from 'react';
+import clsx from 'clsx';
+
+import './Button.scss';
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  appearance?: 'primary' | 'secondary' | 'text' | 'icon' | 'error' | 'success';
+  size?: 'm' | 's';
+};
+
+const Button: FC<PropsWithChildren<ButtonProps>> = (
+  {
+    appearance = 'primary',
+    size = 'm',
+    disabled,
+    children,
+    ...rest
+  },
+) => {
+  const ref = useRef<HTMLButtonElement>(null);
+
+  const buttonClasses = clsx(
+    'btn',
+    `btn--${appearance}`,
+    `btn--${size}`,
+    disabled && 'btn--disabled',
+  );
+
+  return (
+    <button className={buttonClasses} ref={ref} {...rest}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
