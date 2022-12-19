7f9c4e9f6a47d23d51c4f263401833b1caf29f29
Ceiro Kilp <ceiro.kilp@we.ee>
2022-12-19 14:31:18 +0200

    feat: add Tooltip component based on @radix-ui

--- GUI/src/components/index.tsx
+++ GUI/src/components/index.tsx
@@ -4,5 +4,6 @@ import Header from './Header';
 import MainNavigation from './MainNavigation';
 import Icon from './Icon';
 import Track from './Track';
+import Tooltip from './Tooltip';
 
-export { Layout, Button, Header, MainNavigation, Icon, Track };
+export { Layout, Button, Header, MainNavigation, Icon, Track, Tooltip };
