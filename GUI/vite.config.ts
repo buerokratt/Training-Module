9ed0435ec2fa85a8ca1f3a7776612afa639b0b5b
Ceiro Kilp <ceiro.kilp@we.ee>
2022-12-19 10:26:04 +0200

    feat: add prettier and run prettier formatter

--- GUI/vite.config.ts
+++ GUI/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
 
@@ -12,4 +12,4 @@ export default defineConfig({
     target: 'es2015',
     emptyOutDir: true,
   },
-})
+});
