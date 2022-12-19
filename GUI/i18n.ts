f2f591fe7300f06fcf2971c1e82d7c070e580a62
Ceiro Kilp <ceiro.kilp@we.ee>
2022-12-19 11:49:16 +0200

    feat: add i18n support with initial keys + eslint changes

---
+++ GUI/i18n.ts
@@ -0,0 +1,26 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+import commonEN from './translations/en/common.json';
+import commonET from './translations/et/common.json';
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    debug: import.meta.env.NODE_ENV === 'development',
+    fallbackLng: 'et',
+    supportedLngs: ['et', 'en'],
+    resources: {
+      en: {
+        common: commonEN,
+      },
+      et: {
+        common: commonET,
+      },
+    },
+    defaultNS: 'common',
+  });
+
+export default i18n;
