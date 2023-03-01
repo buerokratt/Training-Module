8a87a44a54ab7d072e0164ea1eeb21ad7ea9209b
raul.altmae <raul.altmae@we.ee>
2023-03-01 15:05:46 +0200

    fix: rasa forms search endpoint

--- DSL/OpenSearch/init.sh
+++ DSL/OpenSearch/init.sh
@@ -13,10 +13,14 @@ curl -L -X POST 'http://localhost:9200/_scripts/intents-with-examples-count' -H 
 curl -H "Content-Type: application/x-ndjson" -X PUT "http://localhost:9200/rules" -ku admin:admin --data-binary "@fieldMappings/rules.json"
 curl -H "Content-Type: application/x-ndjson" -X PUT "http://localhost:9200/rules/_bulk" -ku admin:admin --data-binary "@mock/rules.json"
 curl -L -X POST 'http://localhost:9200/_scripts/rule-with-name' -H 'Content-Type: application/json' --data-binary "@templates/rule-with-name.json"
+curl -L -X POST 'http://localhost:9200/_scripts/rules-with-suffix-form' -H 'Content-Type: application/json' -H 'Cookie: customJwtCookie=test' --data-binary "@templates/rules-with-suffix-form.json"
+curl -L -X POST 'http://localhost:9200/_scripts/rules-with-prefix-utter' -H 'Content-Type: application/json' -H 'Cookie: customJwtCookie=test' --data-binary "@templates/rules-with-prefix-utter.json"
 
 #stories
 curl -H "Content-Type: application/x-ndjson" -X PUT "http://localhost:9200/stories" -ku admin:admin --data-binary "@fieldMappings/stories.json"
 curl -H "Content-Type: application/x-ndjson" -X PUT "http://localhost:9200/stories/_bulk" -ku admin:admin --data-binary "@mock/stories.json"
+curl -L -X POST 'http://localhost:9200/_scripts/stories-with-suffix-form' -H 'Content-Type: application/json' -H 'Cookie: customJwtCookie=test' --data-binary "@templates/stories-with-suffix-form.json"
+curl -L -X POST 'http://localhost:9200/_scripts/stories-with-prefix-utter' -H 'Content-Type: application/json' -H 'Cookie: customJwtCookie=test' --data-binary "@templates/stories-with-prefix-utter.json"
 
 #Test stories
 curl -H "Content-Type: application/x-ndjson" -X PUT "http://localhost:9200/test-stories" -ku admin:admin --data-binary "@fieldMappings/test-stories.json"
@@ -26,6 +30,7 @@ curl -L -X POST 'http://localhost:9200/_scripts/test-story-with-name' -H 'Conten
 #Forms
 curl -H "Content-Type: application/x-ndjson" -X PUT "http://localhost:9200/forms" -ku admin:admin --data-binary "@fieldMappings/forms.json"
 curl -H "Content-Type: application/x-ndjson" -X PUT "http://localhost:9200/forms/_bulk" -ku admin:admin --data-binary "@mock/forms.json"
+curl -L -X POST 'http://localhost:9200/_scripts/form-with-slot' -H 'Content-Type: application/json' -H 'Cookie: customJwtCookie=test' --data-binary "@templates/form-with-slot.json"
 
 #Entities
 curl -H "Content-Type: application/x-ndjson" -X PUT "http://localhost:9200/entities" -ku admin:admin --data-binary "@fieldMappings/entities.json"
@@ -39,19 +44,3 @@ curl -H "Content-Type: application/x-ndjson" -X PUT "http://localhost:9200/regex
 #Examples entities
 curl -H "Content-Type: application/x-ndjson" -X PUT "http://localhost:9200/examples-entities" -ku admin:admin --data-binary "@fieldMappings/examples-entities.json"
 curl -H "Content-Type: application/x-ndjson" -X PUT "http://localhost:9200/examples-entities/_bulk" -ku admin:admin --data-binary "@mock/examples-entities.json"
-
-#Rules search
-curl -H "Content-Type: application/x-ndjson" -X PUT "http://localhost:9200/rules-search" -ku admin:admin --data-binary "@fieldMappings/rules-search.json"
-curl -H "Content-Type: application/x-ndjson" -X PUT "http://localhost:9200/rules-search/_bulk" -ku admin:admin --data-binary "@mock/rules-search.json"
-curl -L -X POST 'http://localhost:9200/_scripts/rules-with-suffix-form' -H 'Content-Type: application/json' -H 'Cookie: customJwtCookie=test' --data-binary "@templates/rules-with-suffix-form.json"
-curl -L -X POST 'http://localhost:9200/_scripts/rules-with-prefix-utter' -H 'Content-Type: application/json' -H 'Cookie: customJwtCookie=test' --data-binary "@templates/rules-with-prefix-utter.json"
-
-#Stories search
-curl -H "Content-Type: application/x-ndjson" -X PUT "http://localhost:9200/stories-search" -ku admin:admin --data-binary "@fieldMappings/stories-search.json"
-curl -H "Content-Type: application/x-ndjson" -X PUT "http://localhost:9200/stories-search/_bulk" -ku admin:admin --data-binary "@mock/stories-search.json"
-curl -L -X POST 'http://localhost:9200/_scripts/stories-with-suffix-form' -H 'Content-Type: application/json' -H 'Cookie: customJwtCookie=test' --data-binary "@templates/stories-with-suffix-form.json"
-curl -L -X POST 'http://localhost:9200/_scripts/stories-with-prefix-utter' -H 'Content-Type: application/json' -H 'Cookie: customJwtCookie=test' --data-binary "@templates/stories-with-prefix-utter.json"
-
-#Forms search
-curl -H "Content-Type: application/x-ndjson" -X PUT "http://localhost:9200/forms-search" -ku admin:admin --data-binary "@fieldMappings/forms-search.json"
-curl -H "Content-Type: application/x-ndjson" -X PUT "http://localhost:9200/forms-search/_bulk" -ku admin:admin --data-binary "@mock/forms-search.json"
