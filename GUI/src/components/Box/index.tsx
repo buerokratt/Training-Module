464dce02e6e9f1e247548edd9f2106ddc3c4cd0f
Sergei Rudz <sergei.rudz@we.ee>
2023-03-13 09:17:38 +0200

    feat #60 add props & component exports

--- GUI/src/components/Box/index.tsx
+++ GUI/src/components/Box/index.tsx
@@ -5,11 +5,12 @@ import './Box.scss';
 
 type BoxProps = {
   color?: 'default' | 'blue' | 'yellow' | 'green' | 'red' | 'gray' | 'dark-blue' | 'orange';
+  [rest:string]: any;
 }
 
-const Box = forwardRef<HTMLDivElement, PropsWithChildren<BoxProps>>(({ color = 'default', children }, ref) => {
+const Box = forwardRef<HTMLDivElement, PropsWithChildren<BoxProps>>(({ color = 'default', children, ...rest }, ref,) => {
   return (
-    <div ref={ref} className={clsx(['box', `box--${color}`])}>{children}</div>
+    <div ref={ref} className={clsx(['box', `box--${color}`])} {...rest}>{children}</div>
   );
 });
 
