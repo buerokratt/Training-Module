eef7f953cf48bb839a8b017df7e603a0d309e492
Alar Floren <alar.floren@bpw-consulting.com>
2023-09-19 17:47:31 +0300

    240 - Add documentation for currently existing pipelines Fix init script

--- DSL/Pipelines/init_with_mocks.sh
+++ DSL/Pipelines/init_with_mocks.sh
@@ -8,16 +8,16 @@ for file in $MOCK/data/nlu/* ; do
 done
 
 # rules
-curl -X POST localhost:3010/bulk/rules/rule --form input="`/bin/cat $MOCK/rules.yml`" 
+curl -X POST localhost:3010/bulk/rules/rule --form input="`/bin/cat $MOCK/data/rules.yml`" 
 
 # stories
-curl -X POST localhost:3010/bulk/stories/story --form input="`/bin/cat $MOCK/stories.yml`" 
+curl -X POST localhost:3010/bulk/stories/story --form input="`/bin/cat $MOCK/data/stories.yml`" 
 
 #regexes
-curl -X POST localhost:3010/put/intents/intent --form input="`/bin/cat $MOCK/regex_nlu.yml`" 	
+curl -X POST localhost:3010/put/intents/intent --form input="`/bin/cat $MOCK/data/regex_nlu.yml`" 	
 
 #domain
-curl -v -X POST localhost:3010/bulk/domain --form input=@domain.yml
+curl -v -X POST localhost:3010/bulk/domain --form input=@$MOCK/domain.yml
 
 #domain
-curl -v -X POST localhost:3010/bulk/config --form input=@config.yml
+curl -v -X POST localhost:3010/bulk/config --form input=@$MOCK/config.yml
