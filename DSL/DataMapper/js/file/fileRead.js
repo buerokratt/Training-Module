6b492b32a412b447cea971b3b619ac617a19cd94
KasparMinn <kaspar.minn@gmail.com>
2023-11-24 15:56:55 +0200

    Found datamapper problems with multiple header arrangements after a lot of testing, also fixed response page opening crash

--- DSL/DataMapper/js/file/fileRead.js
+++ DSL/DataMapper/js/file/fileRead.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import fs from 'fs';
 import mime from 'mime-types';
+
 const router = express.Router();
 
 router.post('/', (req, res) => {
@@ -15,23 +16,24 @@ router.post('/', (req, res) => {
         res.status(400).send('Relative paths are not allowed');
         return;
     }
-    const mimeType = mime.lookup(file_path);
-    const name = file_path.split(/(\\|\/)/g).pop();
 
     fs.readFile(file_path, 'utf-8', (err, data) => {
         if (err) {
             res.status(404).send('File not found');
             return;
         }
-        const file = Buffer.from(data).toString('base64');
 
-        res.setHeader('Content-Type', 'application/json');
+        const mimeType = mime.lookup(file_path);
+        const name = file_path.split(/(\\|\/)/g).pop();
+        const file = Buffer.from(data).toString('base64');
 
         const result = {
             name: name,
             file: file,
             mimeType: mimeType
         };
+
+        res.setHeader('Content-Type', 'application/json');
         res.json(result);
     });
 });
