adc2d1e0bc66fce0871ea255b22cc3426ae4e6da
Vassili Moskaljov <112167412+ExiRain@users.noreply.github.com>
2023-12-06 11:23:30 +0200

    #87 REST add rasa SLOT (#318)
    * Updated form, to properly submit values of influendeConversation and type
    
    * Updated value setting for influenceCoversation, updated slots to set false as default value it onchange wasnt triggered
    
    * Added opensearch to add DSL, added slot structuring to slot service, updated Form select component to submit values
    
    * Updated formcheckboxes, formselect , slotdetails
    
    * Updated GUI SlotDetail, added name check,added slot validation to service class, added transaltions

--- GUI/src/services/slots.ts
+++ GUI/src/services/slots.ts
@@ -2,7 +2,8 @@ import api from './api';
 import { Slot, SlotCreateDTO, SlotEditDTO } from 'types/slot';
 
 export async function createSlot(formData: SlotCreateDTO) {
-  const { data } = await api.post<Slot>(`slots`, formData);
+  const slot = validateSlot(formData);
+  const { data } = await api.post<Slot>(`slots/add`, slot);
   return data;
 }
 
@@ -14,4 +15,21 @@ export async function editSlot(id: string | number, formData: SlotEditDTO) {
 export async function deleteSlot(slot: string | number) {
   const { data } = await api.post<void>(`slots/delete`, {slotName: slot});
   return data;
+}
+
+const validateSlot = (formData : SlotCreateDTO | SlotEditDTO) => {
+  return {
+    slot: {
+      [formData.name] : {
+        influence_conversation : formData.influenceConversation || false,
+        type: 'text',
+        mappings: [{
+          type: formData.mappings.type || 'from_text',
+          entity: formData.mappings.entity || 'test',
+          intent: formData.mappings.intent || [],
+          notIntent: formData.mappings.notIntent || []
+        }]
+      }
+    }
+  }
 }
