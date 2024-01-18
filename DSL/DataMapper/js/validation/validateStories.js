eccfed0ffc721d1b02d86ce8fff9017d48bf34df
KasparMinn <kaspar.minn@gmail.com>
2024-01-18 19:34:28 +0200

    Story and rules YML generation fix

---
+++ DSL/DataMapper/js/validation/validateStories.js
@@ -0,0 +1,48 @@
+import express from 'express';
+
+const router = express.Router();
+router.post('/', (req, res) => {
+
+    console.log("REQUEST BODY: ", req.body)
+    const steps = req.body.story.steps;
+    console.log("STEPS: ", steps);
+    const isValid = validateStepsForNoConsecutiveDuplicates(steps);
+    console.log("ISVALID: ", isValid);
+    res.json({ result: isValid });
+
+});
+
+function validateStepsForNoConsecutiveDuplicates(steps) {
+
+    for (let i = 1; i < steps.length; i++) {
+        const currentStep = steps[i];
+        const previousStep = steps[i - 1];
+
+        if (currentStep.entities && previousStep.entities && areConsecutive(currentStep, previousStep)) {
+            const currentEntities = currentStep.entities.map(entity => entity.entity);
+            const previousEntities = previousStep.entities.map(entity => entity.entity);
+
+            if (hasCommonElement(currentEntities, previousEntities)) {
+                return false;
+            }
+        }
+
+        if (currentStep.intent && previousStep.intent && areConsecutive(currentStep, previousStep)) {
+            if (currentStep.intent === previousStep.intent) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+function hasCommonElement(arr1, arr2) {
+    return arr1.some(element => arr2.includes(element));
+}
+
+function areConsecutive(currentStep, previousStep) {
+    return currentStep.start === previousStep.end + 1;
+}
+
+export default router;
