e650cf9978d522a33ce11b9441421fd275aca490
KasparMinn <kaspar.minn@gmail.com>
2024-02-02 20:01:03 +0200

    Story title and loading bar fixes

--- DSL/DataMapper/js/convert/jsonToYamlStories.js
+++ DSL/DataMapper/js/convert/jsonToYamlStories.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import multer from 'multer';
-import yaml, {stringify} from 'yaml';
+import yaml, { stringify } from 'yaml';
 const router = express.Router();
 
 router.post('/', multer().array('file'), async (req, res) => {
@@ -10,11 +10,25 @@ router.post('/', multer().array('file'), async (req, res) => {
             story: entry.story,
             steps: entry.steps.map(step => {
                 const formattedStep = {};
-                if (step.intent) formattedStep.intent = step.intent;
-                if (step.entities) formattedStep.entities = step.entities;
-                if (step.action) formattedStep.action = step.action;
-                if (step.checkpoint) formattedStep.checkpoint = step.checkpoint;
-                if (step.slot_was_set && step.slot_was_set.length > 0) formattedStep.slot_was_set = step.slot_was_set;
+                switch (true) {
+                    case !!step.intent:
+                        formattedStep.intent = step.intent;
+                        break;
+                    case !!step.entities:
+                        formattedStep.entities = step.entities;
+                        break;
+                    case !!step.action:
+                        formattedStep.action = step.action;
+                        break;
+                    case !!step.checkpoint:
+                        formattedStep.checkpoint = step.checkpoint;
+                        break;
+                    case !!step.slot_was_set && step.slot_was_set.length > 0:
+                        formattedStep.slot_was_set = step.slot_was_set;
+                        break;
+                    default:
+                        break;
+                }
                 return formattedStep;
             }).filter(step => Object.keys(step).length > 0),
         })).filter(entry => entry.steps.length > 0),
