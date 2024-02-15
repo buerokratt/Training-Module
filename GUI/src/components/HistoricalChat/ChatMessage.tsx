a4829f804991451d7aec36687423d5e1e4b3bd63
ExiRain <vassilimoskaljov@gmail.com>
2024-02-16 01:34:02 +0200

    Added linkifier dependency and added text decoding for hisotical messages

--- GUI/src/components/HistoricalChat/ChatMessage.tsx
+++ GUI/src/components/HistoricalChat/ChatMessage.tsx
@@ -1,7 +1,8 @@
-import { FC } from 'react';
+import React, { FC } from 'react';
 import { format } from 'date-fns';
 
 import { Message } from 'types/message';
+import Linkifier from "../Chat/linkifier";
 
 type ChatMessageProps = {
   message: Message;
@@ -9,15 +10,22 @@ type ChatMessageProps = {
 }
 
 const ChatMessage: FC<ChatMessageProps> = ({ message, onMessageClick }) => {
-  return (
-    <div className='historical-chat__message'>
-      <div className='historical-chat__message-text'
-           onClick={onMessageClick ? () => onMessageClick(message) : undefined}>{message.content}</div>
-      <time dateTime={message.authorTimestamp} className='historical-chat__message-date'>
-        {format(new Date(message.authorTimestamp), 'HH:ii:ss')}
-      </time>
-    </div>
-  );
+    return (
+        <div className="historical-chat__message">
+            <div
+                className="historical-chat__message-text"
+                onClick={onMessageClick ? () => onMessageClick(message) : undefined}
+            >
+                <Linkifier message={decodeURIComponent(message.content ?? '')} />
+            </div>
+            <time
+                dateTime={message.authorTimestamp}
+                className="historical-chat__message-date"
+            >
+                {format(new Date(message.authorTimestamp), 'HH:mm:ss')}
+            </time>
+        </div>
+    );
 };
 
 export default ChatMessage;
