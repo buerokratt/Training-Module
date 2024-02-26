f52b3842bd279e0256acdfc95ee05c15a76b21d9
Kaspar Minn <kaspar.minn@gmail.com>
2024-02-26 10:06:26 +0200

    111 - Rule (and Story) Creation Back-End Validation Fix (#391)
    * Refactor some rules page things and clarify back-end
    
    * Further story and rule update fixes
    
    * Fix Story and Rules creation on the back-end
    
    * Fix capitalization of names
    
    * Add onSettled

--- GUI/src/pages/Training/Stories/StoriesDetail.tsx
+++ GUI/src/pages/Training/Stories/StoriesDetail.tsx
@@ -141,6 +141,7 @@ const StoriesDetail: FC<{ mode: 'new' | 'edit' }> = ({ mode }) => {
         message: error.message,
       });
     },
+    onSettled: () => setRefreshing(false),
   });
 
   const editStoryMutation = useMutation({
@@ -277,7 +278,7 @@ const StoriesDetail: FC<{ mode: 'new' | 'edit' }> = ({ mode }) => {
 
     setRefreshing(true);
     if (mode === 'new') {
-      addStoryMutation.mutate(data, category);
+      addStoryMutation.mutate({ data, category }); // Pass category here
     }
     if (mode === 'edit' && id) {
       editStoryMutation.mutate({id, data, category});
