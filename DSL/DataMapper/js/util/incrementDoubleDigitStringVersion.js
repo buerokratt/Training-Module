9940bde2a136dc960214eb12b985a581268ae27e
ExiRain <vassilimoskaljov@gmail.com>
2024-02-29 15:25:00 +0200

    added mappers, updated few sql scripts

--- DSL/DataMapper/js/util/incrementDoubleDigitStringVersion.js
+++ DSL/DataMapper/js/util/incrementDoubleDigitStringVersion.js
@@ -4,11 +4,11 @@ const router = express.Router();
 
 router.post('/', (req, res) => {
     const { version } = req.body;
-    const splitVersion = version.split('.');
+    const splitVersion = version.split('_');
     const majorV = splitVersion[0];
     let minorV = parseInt(splitVersion[1]);
     minorV += 1;
-    res.json(`${majorV}.${minorV}`);
+    res.json(`${majorV}_${minorV}`);
 });
 
 export default router;
