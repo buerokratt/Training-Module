e1b512d0eec7ce06f264f85cd584803eeef01f76
baha-a <bhlshrf@gmail.com>
2024-03-14 05:35:23 +0100

    Remove `console.log`s

--- DSL/DataMapper/js/util/removeServicesConnectedToIntent.js
+++ DSL/DataMapper/js/util/removeServicesConnectedToIntent.js
@@ -3,12 +3,9 @@ import express from 'express';
 const router = express.Router();
 
 router.post('/', (req, res) => {
-  console.log(req.body)
   const { connections, services } = req.body;
   const usedServiceIds = new Set(connections.map(x => x.service));
   const unusedServices = services.filter(x => !usedServiceIds.has(x.serviceId));
-
-  console.log(unusedServices)
   
   return res.status(200).send({ 
     unusedServices
