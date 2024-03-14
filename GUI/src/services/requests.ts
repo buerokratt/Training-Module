44106373f9bf368ddb9342ddca432fe87ffda259
baha-a <bhlshrf@gmail.com>
2024-03-14 05:07:38 +0100

    Clean ups

--- GUI/src/services/requests.ts
+++ GUI/src/services/requests.ts
@@ -1,16 +1,5 @@
 import api from './api-dev';
 import { Trigger } from 'types/trigger';
-import { Service } from 'types/service';
-
-export async function getServices() {
-  const { data } = await api.get<Service[]>(`/services/get-services`);
-  return data;
-}
-
-export async function getConnectionRequests() {
-  const { data } = await api.get<Trigger[]>(`/services/connection-requests`);
-  return data;
-}
 
 export async function updateConnectionRequest(request: Trigger, status: "approved" | "declined") {
   const { data } = await api.post<{ response: Trigger[]}>(`/services/respond-to-connection-request`, {
