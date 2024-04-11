08b8468ee17ba96b6ed0af9b0b191014f9d65c17
Varmo <101868197+varmoh@users.noreply.github.com>
2024-04-11 12:20:54 +0300

    Update init_with_mocks.sh

--- DSL/Pipelines/init_with_mocks.sh
+++ DSL/Pipelines/init_with_mocks.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
+# to run it - ./init_with_mocks.sh <url:port>
 
 MOCK=../../mock1
 
+PIPELINES=${1:-"localhost:3010"}
+
 # intents
 for file in $MOCK/data/nlu/* ; do
- curl -X POST localhost:3010/put/intents/intent --form input=@$file
+ curl -X POST $PIPELINES/put/intents/intent --form input=@$file
 done
 
 # rules
-curl -X POST localhost:3010/bulk/rules/rule --form input=@$MOCK/data/rules.yml
+curl -X POST $PIPELINES/bulk/rules/rule --form input=@$MOCK/data/rules.yml
 
 # stories
-curl -X POST localhost:3010/bulk/stories/story --form input=@$MOCK/data/stories.yml
+curl -X POST $PIPELINES/bulk/stories/story --form input=@$MOCK/data/stories.yml
 
 #regexes
 for file in $MOCK/data/regex/* ; do
- curl -X POST localhost:3010/put/regexes/regex --form input=@$file
+ curl -X POST $PIPELINES/put/regexes/regex --form input=@$file
 done
 
 #domain
-curl -v -X POST localhost:3010/bulk/domain --form input=@$MOCK/domain.yml
+curl -v -X POST $PIPELINES/bulk/domain --form input=@$MOCK/domain.yml
 
 #domain
-curl -v -X POST localhost:3010/bulk/config --form input=@$MOCK/config.yml
+curl -v -X POST $PIPELINES/bulk/config --form input=@$MOCK/config.yml
