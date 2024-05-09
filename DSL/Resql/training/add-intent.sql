a8092a93f8075e96359ed69de10af7fd402c0619
ahmedyasser <26207361+1AhmedYasser@users.noreply.github.com>
2024-05-09 03:12:17 +0300

    Fixed response.data

--- DSL/Resql/training/add-intent.sql
+++ DSL/Resql/training/add-intent.sql
@@ -1,2 +1,6 @@
 INSERT INTO intent (intent, created, status)
-VALUES (:intent, CURRENT_TIMESTAMP, :status);
+VALUES (:intent, CURRENT_TIMESTAMP, :status)
+ON CONFLICT (id) DO UPDATE
+SET intent = EXCLUDED.intent,
+    created = EXCLUDED.created,
+    status = EXCLUDED.status;
