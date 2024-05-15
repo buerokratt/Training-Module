4e1efe48389b3037d30304af1b66b17c088f9ce1
baha-a <bhlshrf@gmail.com>
2024-05-15 17:26:08 +0300

    Remove UPDATE from `add-intent.sql`

--- DSL/Resql/training/add-intent.sql
+++ DSL/Resql/training/add-intent.sql
@@ -1,6 +1,2 @@
 INSERT INTO intent (intent, created, status)
-VALUES (:intent, CURRENT_TIMESTAMP, :status)
-ON CONFLICT (id) DO UPDATE
-SET intent = EXCLUDED.intent,
-    created = EXCLUDED.created,
-    status = EXCLUDED.status;
+VALUES (:intent, CURRENT_TIMESTAMP, :status);
