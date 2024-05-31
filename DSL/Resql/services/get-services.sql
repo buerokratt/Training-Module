e152a16723f48c5e580d69d309e1351c853efe39
baha-a <bhlshrf@gmail.com>
2024-05-31 14:42:18 +0200

    Add pagination to SQL Queries

--- DSL/Resql/services/get-services.sql
+++ DSL/Resql/services/get-services.sql
@@ -1,3 +1,17 @@
-SELECT DISTINCT name, service_id
+WITH MaxServices AS (
+	SELECT max(id) AS maxId
+	FROM services
+	GROUP BY service_id
+)
+SELECT
+  name,
+  service_id,
+  CEIL(COUNT(*) OVER() / :page_size::DECIMAL) AS total_pages
 FROM services
-ORDER BY name ASC;
+JOIN MaxServices ON maxId = id
+WHERE deleted IS NOT NULL
+AND (:search IS NULL OR name LIKE ('%' || :search || '%'))
+ORDER BY
+  CASE WHEN :sorting = 'name asc' THEN name END ASC,
+  CASE WHEN :sorting = 'name desc' THEN name END DESC
+OFFSET ((GREATEST(:page, 1) - 1) * :page_size) LIMIT :page_size;
--- DSL/Resql/training/get-requested-service-triggers.sql
+++ DSL/Resql/training/get-requested-service-triggers.sql
@@ -2,8 +2,17 @@ SELECT intent,
        service,
        MAX(service_name) AS service_name,
        MAX(created) AS requested_at,
-       MAX(author_role) as author_role 
+       MAX(author_role) as author_role,
+       CEIL(COUNT(*) OVER() / :page_size::DECIMAL) AS total_pages
 FROM service_trigger
 GROUP BY intent, service
 HAVING MAX(status) = 'pending'
-AND MAX("author_role") != 'trainer';
+AND MAX("author_role") != 'trainer'
+ORDER BY
+   CASE WHEN :sorting = 'intent asc' THEN intent END ASC,
+   CASE WHEN :sorting = 'intent desc' THEN intent END DESC,
+   CASE WHEN :sorting = 'serviceName asc' THEN MAX(service_name) END ASC,
+   CASE WHEN :sorting = 'serviceName desc' THEN MAX(service_name) END DESC,
+   CASE WHEN :sorting = 'requestedAt asc' THEN MAX(created) END ASC,
+   CASE WHEN :sorting = 'requestedAt desc' THEN MAX(created) END DESC
+OFFSET ((GREATEST(:page, 1) - 1) * :page_size) LIMIT :page_size;
