e7c13c9c4372c0a7072b7eef634dc11ff7a56509
baha-a <bhlshrf@gmail.com>
2024-06-11 13:01:45 +0200

    Update environment variables in nginx script

--- GUI/nginx/scripts/env.sh
+++ GUI/nginx/scripts/env.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
-for envrow in $(printenv);
-do
-    IFS='=' read -r key value <<< "${envrow}"
+for key in $(compgen -e); do
     if [[ $key == "REACT_APP_"* ]]; then
-    for file in $(find /usr/share/nginx/html -type f \( -name '*.js' -o -name '*.css' \))
-    do
-       sed -i "s|{}.${key}|\"${value}\"|g" $file;
-    done
+        value=$(eval echo \$$key)
+        if [[ $key == "REACT_APP_MENU_JSON" ]]; then
+            wrapped_value="'${value}'"
+        else
+            wrapped_value="\"${value}\""
+        fi
+        for file in $(find /usr/share/nginx/html -type f \( -name '*.js' -o -name '*.css' \)); do
+            sed -i "s|{}.${key}|${wrapped_value}|g" "$file"
+            sed -i "s|({}).${key}|${wrapped_value}|g" "$file"
+        done
     fi
 done
-[ -z "$@" ] && nginx -g 'daemon off;' || $@
+[ -z "$@" ] && nginx -g 'daemon off;' || "$@"
