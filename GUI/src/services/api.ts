6ea51e9240bb5ac773a63bd8af65db17d9556aea
karlerik.hein <karlerik.hein@netgroup.com>
2024-08-08 12:19:05 +0300

    feat: Axios interceptor now handles 409 via translations

--- GUI/src/services/api.ts
+++ GUI/src/services/api.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosError } from 'axios';
+import {useTranslation} from "react-i18next";
 
 const instance = axios.create({
   baseURL: import.meta.env.REACT_APP_RUUTER_URL + 'rasa/',
@@ -16,6 +17,11 @@ instance.interceptors.response.use(
   },
   (error: AxiosError) => {
     import.meta.env.DEBUG_ENABLED && console.log(error);
+    const { t } = useTranslation();
+    if (error.response?.status === 409) {
+      let message = t('axios.error.conflict');
+      return Promise.reject(new Error(message));
+    }
     return Promise.reject(new Error(error.message));
   }
 );
