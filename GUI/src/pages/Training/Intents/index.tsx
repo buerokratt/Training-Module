e154163841bad22f51d8bd3a444ca3ee8f1c1081
Joonas Roosalu <104758060+joonasroosalung@users.noreply.github.com>
2024-08-26 11:51:36 +0300

    feat: make vertical tabs list scrollable in vertical tabs #819 (#637)
    * feat: make vertical tabs list scrollable in vertical tabs #819
    
    * feat: remove comment #819

--- GUI/src/pages/Training/Intents/index.tsx
+++ GUI/src/pages/Training/Intents/index.tsx
@@ -755,23 +755,24 @@ const Intents: FC = () => {
               </Track>
             </div>
 
-            {filteredIntents.map((intent, index) => (
-              <Tabs.Trigger
-                key={`${intent}-${index}`}
-                className="vertical-tabs__trigger"
-                value={intent.id}
-              >
-                <Track gap={16}>
+            <div className="vertical-tabs__list-scrollable">
+              {filteredIntents.map((intent, index) => (
+                <Tabs.Trigger
+                  key={`${intent}-${index}`}
+                  className="vertical-tabs__trigger"
+                  value={intent.id}
+                >
+                  <Track gap={16}>
                   <span style={{ flex: 1 }}>
                     {intent.id.replace(/_/g, ' ')}
                   </span>
-                  <Tooltip content={t('training.intents.amountOfExamples')}>
+                    <Tooltip content={t('training.intents.amountOfExamples')}>
                     <span style={{ color: '#5D6071' }}>
                       {intent.examplesCount}
                     </span>
-                  </Tooltip>
-                  {intent.inModel ? (
-                    <Tooltip content={t('training.intents.intentInModel')}>
+                    </Tooltip>
+                    {intent.inModel ? (
+                      <Tooltip content={t('training.intents.intentInModel')}>
                       <span style={{ display: 'flex', alignItems: 'center' }}>
                         <Icon
                           icon={
@@ -782,13 +783,14 @@ const Intents: FC = () => {
                           }
                         />
                       </span>
-                    </Tooltip>
-                  ) : (
-                    <span style={{ display: 'block', width: 16 }}></span>
-                  )}
-                </Track>
-              </Tabs.Trigger>
-            ))}
+                      </Tooltip>
+                    ) : (
+                      <span style={{ display: 'block', width: 16 }}></span>
+                    )}
+                  </Track>
+                </Tabs.Trigger>
+              ))}
+            </div>
           </Tabs.List>
 
           {selectedIntent && (
