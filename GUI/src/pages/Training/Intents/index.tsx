bd7f2fd8cfb8d3db79119fd42f4bec38df76f27c
Igor Krupenja <igor.krupenja@gmail.com>
2024-11-18 13:23:41 +0200

    Fix build

--- GUI/src/pages/Training/Intents/index.tsx
+++ GUI/src/pages/Training/Intents/index.tsx
@@ -4,7 +4,7 @@ import { useTranslation } from 'react-i18next';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import * as Tabs from '@radix-ui/react-tabs';
 import { format } from 'date-fns';
-import { AxiosError, AxiosError } from 'axios';
+import { AxiosError } from 'axios';
 import {MdOutlineModeEditOutline, MdOutlineSave,} from 'react-icons/md';
 
 import {Button, Dialog, FormInput, FormTextarea, Icon, Switch, Tooltip, Track} from 'components';
