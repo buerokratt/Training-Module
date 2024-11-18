b9aebb22d25ebca078aa22d454f82aab0d229114
Igor Krupenja <igor.krupenja@gmail.com>
2024-11-18 13:19:17 +0200

    Fix gs

--- GUI/src/pages/Training/Intents/index.tsx
+++ GUI/src/pages/Training/Intents/index.tsx
@@ -33,6 +33,8 @@ import { Rule, RuleDTO } from '../../../types/rule';
 import { addStoryOrRule, deleteStoryOrRule } from '../../../services/stories';
 import IntentTabList from './IntentTabList';
 import useStore from "../../../store/store";
+import { blob } from 'stream/consumers';
+import console from 'console';
 
 type Response = {
   name: string;
@@ -458,15 +460,24 @@ const Intents: FC = () => {
   const intentDownloadMutation = useMutation({
     mutationFn: (intentModelData: { intentName: string }) =>
       downloadExamples(intentModelData),
-    onSuccess: (data) => {
+    onSuccess: async (data) => {
       // @ts-ignore
       const blob = new Blob([data], { type: 'text/csv' });
-      const url = window.URL.createObjectURL(blob);
-      const a = document.createElement('a');
-      a.href = url;
-      a.download = selectedIntent?.id + '.csv';
-      a.click();
-      window.URL.revokeObjectURL(url);
+      const fileName = selectedIntent?.id + '.csv';
+
+      if (window.showSaveFilePicker) {
+        const handle = await window.showSaveFilePicker({ suggestedName: fileName });
+        const writable = await handle.createWritable();
+        await writable.write(blob);
+        writable.close();
+      } else {
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = fileName;
+        a.click();
+        window.URL.revokeObjectURL(url);
+      }
 
       toast.open({
         type: 'success',
@@ -475,11 +486,13 @@ const Intents: FC = () => {
       });
     },
     onError: (error: AxiosError) => {
-      toast.open({
-        type: 'error',
-        title: t('global.notificationError'),
-        message: error.message,
-      });
+      if (error.name !== 'AbortError') {
+        toast.open({
+          type: 'error',
+          title: t('global.notificationError'),
+          message: error.message,
+        });      
+      }
     },
   });
 
