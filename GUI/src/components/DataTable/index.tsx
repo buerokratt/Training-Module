4ea0d49778906e29ce6af21773645891e8d2d80b
Igor Krupenja <igor.krupenja@gmail.com>
2025-01-05 16:02:50 +0200

    Revert "Revert unrelated changes"
    This reverts commit 17f5b3dc5f57e95587cbb3837f230cea311fbde6.

--- GUI/src/components/DataTable/index.tsx
+++ GUI/src/components/DataTable/index.tsx
@@ -13,19 +13,11 @@ import {
   PaginationState,
   TableMeta,
   Row,
-  RowData, ColumnFiltersState,
+  RowData,
+  ColumnFiltersState,
 } from '@tanstack/react-table';
-import {
-  RankingInfo,
-  rankItem,
-} from '@tanstack/match-sorter-utils';
-import {
-  MdUnfoldMore,
-  MdExpandMore,
-  MdExpandLess,
-  MdOutlineEast,
-  MdOutlineWest,
-} from 'react-icons/md';
+import { RankingInfo, rankItem } from '@tanstack/match-sorter-utils';
+import { MdUnfoldMore, MdExpandMore, MdExpandLess, MdOutlineEast, MdOutlineWest } from 'react-icons/md';
 import clsx from 'clsx';
 import { useSearchParams } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
@@ -33,6 +25,7 @@ import { useTranslation } from 'react-i18next';
 import { Icon, Track } from 'components';
 import Filter from './Filter';
 import './DataTable.scss';
+import { FetchNextPageOptions, InfiniteQueryObserverResult } from '@tanstack/react-query';
 
 type DataTableProps = {
   data: any;
@@ -54,6 +47,15 @@ type DataTableProps = {
   meta?: TableMeta<any>;
   setSelectedRow?: (row: Row<any>) => void;
   pageSizeOptions?: number[];
+  isFetching?: boolean;
+  fetchNextPage?: (options?: FetchNextPageOptions) => Promise<
+    InfiniteQueryObserverResult<
+      {
+        response: unknown[];
+      },
+      unknown
+    >
+  >;
 };
 
 type ColumnMeta = {
@@ -92,33 +94,47 @@ const fuzzyFilter: FilterFn<any> = (row, columnId, value, addMeta) => {
   return itemRank.passed;
 };
 
-const DataTable: FC<DataTableProps> = (
-  {
-    data,
-    columns,
-    isClientSide = true,
-    tableBodyPrefix,
-    sortable,
-    filterable,
-    sorting,
-    pagination,
-    setPagination,
-    setSorting,
-    globalFilter,
-    setGlobalFilter,
-    columnVisibility,
-    setColumnVisibility,
-    disableHead,
-    pagesCount,
-    meta,
-    setSelectedRow,
-    pageSizeOptions = [10, 20, 30, 40, 50],
-  },
-) => {
+const DataTable: FC<DataTableProps> = ({
+  data,
+  columns,
+  isClientSide = true,
+  tableBodyPrefix,
+  sortable,
+  filterable,
+  sorting,
+  pagination,
+  setPagination,
+  setSorting,
+  globalFilter,
+  setGlobalFilter,
+  columnVisibility,
+  setColumnVisibility,
+  disableHead,
+  pagesCount,
+  meta,
+  setSelectedRow,
+  pageSizeOptions = [10, 20, 30, 40, 50],
+  isFetching,
+  fetchNextPage,
+}) => {
   const id = useId();
   const { t } = useTranslation();
   const [searchParams, setSearchParams] = useSearchParams();
   const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>([]);
+
+  const fetchMoreOnBottomReached = React.useCallback(
+    (containerRefElement?: HTMLDivElement | null) => {
+      if (containerRefElement) {
+        const { scrollHeight, scrollTop, clientHeight } = containerRefElement;
+        // Once the user has scrolled within 500px of the bottom of the table
+        if (scrollHeight - scrollTop - clientHeight < 500 && !isFetching && fetchNextPage) {
+          fetchNextPage();
+        }
+      }
+    },
+    [fetchNextPage, isFetching]
+  );
+
   const table = useReactTable({
     data,
     columns,
@@ -166,80 +182,101 @@ const DataTable: FC<DataTableProps> = (
       pages.push(i);
     }
     return pages;
-  }
+  };
   const pageIndexes = getShownPageIndexes();
 
   return (
     <div>
-      <div className='data-table__scrollWrapper'>
-      <table className='data-table'>
-        {!disableHead && (
-          <thead>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <tr key={headerGroup.id}>
-              {headerGroup.headers.map((header) => (
-                  <th key={header.id} style={{
-                    width: header.column.columnDef.size,
-                    position: header.column.columnDef.meta?.sticky ? 'sticky' : undefined,
-                    left: header.column.columnDef.meta?.sticky === 'left' ? `${header.column.getAfter('left') * 0.675}px` : undefined,
-                    right: header.column.columnDef.meta?.sticky === 'right' ? `${header.column.getAfter('right') * 0.675}px` : undefined,
-                    backgroundColor: 'white',
-                    zIndex: header.column.columnDef.meta?.sticky ? 1 : 0
-                  }}>
-                  {header.isPlaceholder ? null : (
-                    <Track gap={8}>
-                      {sortable && header.column.getCanSort() && (
-                        <button onClick={header.column.getToggleSortingHandler()}>
-                          {{
-                            asc: <Icon icon={<MdExpandMore fontSize={20} />} size='medium' />,
-                            desc: <Icon icon={<MdExpandLess fontSize={20} />} size='medium' />,
-                          }[header.column.getIsSorted() as string] ?? (
-                            <Icon icon={<MdUnfoldMore fontSize={22} />} size='medium' />
+      <div className="data-table__scrollWrapper" onScroll={(e) => fetchMoreOnBottomReached(e.currentTarget)}>
+        <table className="data-table">
+          {!disableHead && (
+            <thead>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <tr key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => (
+                    <th
+                      key={header.id}
+                      style={{
+                        width: header.column.columnDef.size,
+                        position: header.column.columnDef.meta?.sticky ? 'sticky' : undefined,
+                        left:
+                          header.column.columnDef.meta?.sticky === 'left'
+                            ? `${header.column.getAfter('left') * 0.675}px`
+                            : undefined,
+                        right:
+                          header.column.columnDef.meta?.sticky === 'right'
+                            ? `${header.column.getAfter('right') * 0.675}px`
+                            : undefined,
+                        backgroundColor: 'white',
+                        zIndex: header.column.columnDef.meta?.sticky ? 1 : 0,
+                      }}
+                    >
+                      {header.isPlaceholder ? null : (
+                        <Track gap={8}>
+                          {sortable && header.column.getCanSort() && (
+                            <button onClick={header.column.getToggleSortingHandler()}>
+                              {{
+                                asc: <Icon icon={<MdExpandMore fontSize={20} />} size="medium" />,
+                                desc: <Icon icon={<MdExpandLess fontSize={20} />} size="medium" />,
+                              }[header.column.getIsSorted() as string] ?? (
+                                <Icon icon={<MdUnfoldMore fontSize={22} />} size="medium" />
+                              )}
+                            </button>
                           )}
-                        </button>
+                          {flexRender(header.column.columnDef.header, header.getContext())}
+                          {filterable && header.column.getCanFilter() && (
+                            <Filter column={header.column} table={table} />
+                          )}
+                        </Track>
                       )}
-                      {flexRender(header.column.columnDef.header, header.getContext())}
-                      {filterable && header.column.getCanFilter() && (
-                        <Filter column={header.column} table={table} />
-                      )}
-                    </Track>
-                  )}
-                </th>
+                    </th>
+                  ))}
+                </tr>
               ))}
-            </tr>
-          ))}
-          </thead>
-        )}
-        <tbody>
-        {tableBodyPrefix}
-        {table.getRowModel().rows.map((row) => (
-          <tr 
-            key={row.id} 
-            onClick={() => setSelectedRow && setSelectedRow(row)}
-            style={table.options.meta?.getRowStyles(row)}
-          >
-            {row.getVisibleCells().map((cell) => (
-                <td key={cell.id} style={{
-                  position: cell.column.columnDef.meta?.sticky ? 'sticky' : undefined,
-                  left: cell.column.columnDef.meta?.sticky === 'left' ? `${cell.column.getAfter('left') * 0.675}px` : undefined,
-                  right: cell.column.columnDef.meta?.sticky === 'right' ? `${cell.column.getAfter('right') * 0.675}px` : undefined,
-                  backgroundColor: 'white', zIndex: cell.column.columnDef.meta?.sticky ? 1 : 0
-                }}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</td>
+            </thead>
+          )}
+          <tbody>
+            {tableBodyPrefix}
+            {table.getRowModel().rows.map((row) => (
+              <tr
+                key={row.id}
+                onClick={() => setSelectedRow && setSelectedRow(row)}
+                style={table.options.meta?.getRowStyles(row)}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <td
+                    key={cell.id}
+                    style={{
+                      position: cell.column.columnDef.meta?.sticky ? 'sticky' : undefined,
+                      left:
+                        cell.column.columnDef.meta?.sticky === 'left'
+                          ? `${cell.column.getAfter('left') * 0.675}px`
+                          : undefined,
+                      right:
+                        cell.column.columnDef.meta?.sticky === 'right'
+                          ? `${cell.column.getAfter('right') * 0.675}px`
+                          : undefined,
+                      backgroundColor: 'white',
+                      zIndex: cell.column.columnDef.meta?.sticky ? 1 : 0,
+                    }}
+                  >
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                ))}
+              </tr>
             ))}
-          </tr>
-        ))}
-        </tbody>
-      </table>
+          </tbody>
+        </table>
       </div>
       {pagination && (
-        <div className='data-table__pagination-wrapper'>
-          {(table.getPageCount() * table.getState().pagination.pageSize) > table.getState().pagination.pageSize && (
-            <div className='data-table__pagination'>
+        <div className="data-table__pagination-wrapper">
+          {table.getPageCount() * table.getState().pagination.pageSize > table.getState().pagination.pageSize && (
+            <div className="data-table__pagination">
               <button
-                className='previous'
+                className="previous"
                 onClick={() => {
                   table.previousPage();
-                  setSearchParams(params => {
+                  setSearchParams((params) => {
                     params.set('page', String(Number(searchParams.get('page') ?? '1') - 1));
                     return params;
                   });
@@ -248,8 +285,8 @@ const DataTable: FC<DataTableProps> = (
               >
                 <MdOutlineWest />
               </button>
-              <nav role='navigation' aria-label={t('global.paginationNavigation') || ''}>
-                <ul className='links'>
+              <nav role="navigation" aria-label={t('global.paginationNavigation') || ''}>
+                <ul className="links">
                   {pageIndexes[0] > 0 && (
                     <>
                       <li key={`${id}-0`}>
@@ -273,12 +310,12 @@ const DataTable: FC<DataTableProps> = (
                   {pageIndexes.map((index) => (
                     <li
                       key={`${id}-${index}`}
-                      className={clsx({ 'active': table.getState().pagination.pageIndex === index })}
+                      className={clsx({ active: table.getState().pagination.pageIndex === index })}
                     >
                       <button
                         onClick={() => {
                           table.setPageIndex(index);
-                          setSearchParams(params => {
+                          setSearchParams((params) => {
                             params.set('page', String(index + 1));
                             return params;
                           });
@@ -313,10 +350,10 @@ const DataTable: FC<DataTableProps> = (
                 </ul>
               </nav>
               <button
-                className='next'
+                className="next"
                 onClick={() => {
                   table.nextPage();
-                  setSearchParams(params => {
+                  setSearchParams((params) => {
                     params.set('page', String(Number(searchParams.get('page') ?? '1') + 1));
                     return params;
                   });
@@ -327,16 +364,16 @@ const DataTable: FC<DataTableProps> = (
               </button>
             </div>
           )}
-          <div className='data-table__page-size'>
+          <div className="data-table__page-size">
             <label htmlFor={id}>{t('global.resultCount')}</label>
             <select
               id={id}
               value={table.getState().pagination.pageSize}
-              onChange={e => {
+              onChange={(e) => {
                 table.setPageSize(Number(e.target.value));
               }}
             >
-              {pageSizeOptions.map(pageSize => (
+              {pageSizeOptions.map((pageSize) => (
                 <option key={pageSize} value={pageSize}>
                   {pageSize}
                 </option>
