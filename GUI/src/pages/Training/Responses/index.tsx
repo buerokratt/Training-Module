2879802e7d11cd9110be5cf41b8a85a54d73515c
Igor Krupenja <igor.krupenja@gmail.com>
2025-01-06 19:46:25 +0200

    Fix response

--- GUI/src/pages/Training/Responses/index.tsx
+++ GUI/src/pages/Training/Responses/index.tsx
@@ -1,24 +1,32 @@
-import {FC, useMemo, useState} from 'react';
-import {useTranslation} from 'react-i18next';
-import {useMutation, useQuery, useQueryClient} from '@tanstack/react-query';
-import {createColumnHelper} from '@tanstack/react-table';
-import {Controller, useForm} from 'react-hook-form';
-import {AxiosError} from 'axios';
-import {MdDeleteOutline, MdOutlineModeEditOutline, MdOutlineSave} from 'react-icons/md';
+import { FC, useMemo, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { createColumnHelper } from '@tanstack/react-table';
+import { Controller, useForm } from 'react-hook-form';
+import { AxiosError } from 'axios';
+import { MdDeleteOutline, MdOutlineModeEditOutline, MdOutlineSave } from 'react-icons/md';
 
-import {Button, Card, DataTable, Dialog, FormInput, FormTextarea, Icon, Label, Track} from 'components';
-import {RESPONSE_TEXT_LENGTH} from 'constants/config';
-import type {Responses as ResponsesType} from 'types/response';
-import type {Dependencies as DependenciesType} from 'types/dependencises';
+import { Button, Card, DataTable, Dialog, FormInput, FormTextarea, Icon, Label, Track } from 'components';
+import { RESPONSE_TEXT_LENGTH } from 'constants/config';
+import type { ResponseData } from 'types/response';
+import type { Dependencies as DependenciesType } from 'types/dependencises';
 import useDocumentEscapeListener from 'hooks/useDocumentEscapeListener';
-import {useToast} from 'hooks/useToast';
-import {deleteResponse, editResponse} from 'services/responses';
-import LoadingDialog from "../../../components/LoadingDialog";
+import { useToast } from 'hooks/useToast';
+import { deleteResponse, editResponse, getResponses } from 'services/responses';
+import LoadingDialog from '../../../components/LoadingDialog';
 import i18n from '../../../../i18n';
 import withAuthorization, { ROLES } from 'hoc/with-authorization';
+import { useInfinitePagination } from 'hooks/useInfinitePagination';
+import { flattenPaginatedData } from 'utils/api-utils';
 
 type NewResponse = {
   name: string;
+  text: string;
+};
+
+interface Response {
+  id: number;
+  response: string;
   text: string;
 }
 
@@ -26,26 +34,37 @@ const Responses: FC = () => {
   const { t } = useTranslation();
   const toast = useToast();
   const queryClient = useQueryClient();
+  // todo add debounce
+  const [filter, setFilter] = useState('');
   const { data: dependencies } = useQuery<DependenciesType>({
     queryKey: ['responses/dependencies'],
   });
-  const { data: responses, refetch } = useQuery<ResponsesType>({
-    queryKey: ['responses-list'],
+  // const { data: responses, refetch } = useQuery<ResponsesType>({
+  //   queryKey: ['responses-list'],
+  // });
+
+  const { data, refetch, fetchNextPage, isFetching } = useInfinitePagination<ResponseData>({
+    queryKey: ['responses-list', filter],
+    fetchFn: getResponses,
+    filter,
   });
+  console.log('data', data);
+  const responses = useMemo(() => flattenPaginatedData(data), [data]);
+  console.log('responses', responses);
+
   const [addFormVisible, setAddFormVisible] = useState(false);
-  const [editableRow, setEditableRow] = useState<{ id: string; text: string; } | null>(null);
+  const [editableRow, setEditableRow] = useState<{ id: string; text: string } | null>(null);
   const [deletableRow, setDeletableRow] = useState<string | null>(null);
-  const [filter, setFilter] = useState('');
-  const { register,control, handleSubmit, resetField } = useForm<NewResponse>();
-  const [formattedResponses, setFormattedResponses] = useState([]);
+  const { register, control, handleSubmit, resetField } = useForm<NewResponse>();
+  const [formattedResponses, setFormattedResponses] = useState<Response[]>([]);
   const [refreshing, setRefreshing] = useState(false);
 
   useMemo(() => {
     if (responses && responses.length > 0) {
-      const formattedData = responses[0].response.map((r, i) => ({
-        id: i,
-        response: r.name,
-        text: r.text,
+      const formattedData = responses.map((r, i) => ({
+        id: i ?? 'HUI',
+        response: r.name ?? 'HUI',
+        text: r.text ?? 'HUI',
       }));
       setFormattedResponses(formattedData);
     } else {
@@ -61,7 +80,7 @@ const Responses: FC = () => {
   }
 
   const responseSaveMutation = useMutation({
-    mutationFn: ({ id, text }: { id: string, text: string }) => editResponse(id,  text),
+    mutationFn: ({ id, text }: { id: string; text: string }) => editResponse(id, text),
     onMutate: async () => {
       await queryClient.cancelQueries(['responses-list']);
       setRefreshing(true);
@@ -81,11 +100,11 @@ const Responses: FC = () => {
         title: t('global.notificationError'),
         message: error.message,
       });
-      setRefreshing(false)
+      setRefreshing(false);
     },
     onSettled: () => {
       setEditableRow(null);
-      setRefreshing(false)
+      setRefreshing(false);
     },
   });
 
@@ -109,18 +128,19 @@ const Responses: FC = () => {
         title: t('global.notificationError'),
         message: error.message,
       });
-      setRefreshing(false)
+      setRefreshing(false);
     },
     onSettled: async () => {
       const updatedResponses = formattedResponses.filter((r) => r.response !== deletableRow);
       setFormattedResponses(updatedResponses);
       setDeletableRow(null);
-      setRefreshing(false)
+      setRefreshing(false);
     },
   });
 
   const newResponseMutation = useMutation({
-    mutationFn: ({ name, text}: { name: string, text: string}) => editResponse("utter_" + name.trim().replace(/\s+/g, '_'),  text, false),
+    mutationFn: ({ name, text }: { name: string; text: string }) =>
+      editResponse('utter_' + name.trim().replace(/\s+/g, '_'), text, false),
     onMutate: async () => {
       await queryClient.invalidateQueries(['responses-list']);
       setRefreshing(true);
@@ -141,12 +161,12 @@ const Responses: FC = () => {
         title: t('global.notificationError'),
         message: error.message,
       });
-      setRefreshing(false)
+      setRefreshing(false);
     },
     onSettled: () => {
       setTimeout(() => refetch(), 1100);
       setRefreshing(false);
-    }
+    },
   });
 
   const handleNewResponseSubmit = handleSubmit(async (data) => {
@@ -157,67 +177,73 @@ const Responses: FC = () => {
     setEditableRow(response);
   };
 
-  const columnHelper = createColumnHelper<typeof formattedResponses[0]>();
+  const columnHelper = createColumnHelper<(typeof formattedResponses)[0]>();
 
   const getRules = (responseId: string) => {
-    const dependency = dependencies?.response.find(d => d.name === responseId);
+    const dependency = dependencies?.response.find((d) => d.name === responseId);
     return dependency?.rules.map((name, i) => <p key={name}>{name}</p>);
   };
 
   const getStories = (responseId: string) => {
-    const dependency = dependencies?.response.find(d => d.name === responseId);
+    const dependency = dependencies?.response.find((d) => d.name === responseId);
     return dependency?.stories.map((name, i) => <p key={name}>{name}</p>);
   };
 
-  const responsesColumns = useMemo(() => [
-    columnHelper.accessor('response', {
-      header: t('training.responses.response') || '',
-      meta: {
-        size: '1%',
-      },
-    }),
-    columnHelper.display({
-      id: 'dependencies',
-      cell: (props) => buildDependenciesCell(
-        getRules(props.row.original.response),
-        getStories(props.row.original.response)
-      ),
-      meta: {
-        size: '167px',
-      },
-    }),
-    columnHelper.accessor('text', {
-      header: '',
-      cell: (props) => buildTextCell(
-        props.getValue(),
-        editableRow?.id === props.row.original.response,
-        setEditingTrainingTitle,
-      ),
-      enableSorting: false,
-    }),
-    columnHelper.display({
-      header: '',
-      cell: (props) => buildSaveCell(
-        editableRow?.id === props.row.original.response,
-        () => editableRow && responseSaveMutation.mutate({id: editableRow.id , text: editingTrainingTitle.replaceAll(/\n{2,}/g, '\n').replaceAll('\n', '\\n\\n') ?? props.row.original.text.replaceAll(/\n{2,}/g, '\n').replaceAll('\n', '\\n\\n')}),
-        () => {
-          setEditingTrainingTitle(props.row.original.text);
-          handleEditableRow({ id: props.row.original.response, text: props.row.original.text });
+  const responsesColumns = useMemo(
+    () => [
+      columnHelper.accessor('response', {
+        header: t('training.responses.response') || '',
+        meta: {
+          size: '1%',
         },
-      ),
-      id: 'edit',
-      meta: {
-        size: '1%',
-      },
-    }),
-    columnHelper.display({
-      id: 'delete',
-      cell: (props) => buildDeleteCell(() => setDeletableRow(props.row.original.response)),
-      meta: {
-        size: '1%',
-      },
-    }),
-  ], [columnHelper, editableRow, t]);
+      }),
+      columnHelper.display({
+        id: 'dependencies',
+        cell: (props) =>
+          buildDependenciesCell(getRules(props.row.original.response), getStories(props.row.original.response)),
+        meta: {
+          size: '167px',
+        },
+      }),
+      columnHelper.accessor('text', {
+        header: '',
+        cell: (props) =>
+          buildTextCell(props.getValue(), editableRow?.id === props.row.original.response, setEditingTrainingTitle),
+        enableSorting: false,
+      }),
+      columnHelper.display({
+        header: '',
+        cell: (props) =>
+          buildSaveCell(
+            editableRow?.id === props.row.original.response,
+            () =>
+              editableRow &&
+              responseSaveMutation.mutate({
+                id: editableRow.id,
+                text:
+                  editingTrainingTitle.replaceAll(/\n{2,}/g, '\n').replaceAll('\n', '\\n\\n') ??
+                  props.row.original.text.replaceAll(/\n{2,}/g, '\n').replaceAll('\n', '\\n\\n'),
+              }),
+            () => {
+              setEditingTrainingTitle(props.row.original.text);
+              handleEditableRow({ id: props.row.original.response, text: props.row.original.text });
+            }
+          ),
+        id: 'edit',
+        meta: {
+          size: '1%',
+        },
+      }),
+      columnHelper.display({
+        id: 'delete',
+        cell: (props) => buildDeleteCell(() => setDeletableRow(props.row.original.response)),
+        meta: {
+          size: '1%',
+        },
+      }),
+    ],
+    [columnHelper, editableRow, t]
+  );
 
   if (!responses) return <>Loading...</>;
 
@@ -229,12 +255,17 @@ const Responses: FC = () => {
         <Track gap={16}>
           <FormInput
             label={t('training.responses.searchResponse')}
-            name='responseSearch'
+            name="responseSearch"
             placeholder={t('training.responses.searchResponse') + '...'}
             hideLabel
             onChange={(e) => setFilter(e.target.value)}
           />
-          <Button onClick={() => {setAddFormVisible(true); setEditingTrainingTitle("")}}>
+          <Button
+            onClick={() => {
+              setAddFormVisible(true);
+              setEditingTrainingTitle('');
+            }}
+          >
             {t('global.add')}
           </Button>
         </Track>
@@ -242,34 +273,36 @@ const Responses: FC = () => {
 
       {addFormVisible && (
         <Card>
-          <Track justify='between' gap={16}>
+          <Track justify="between" gap={16}>
             <div style={{ flex: 1 }}>
               <Track gap={16}>
                 <p>utter_</p>
-                <FormInput
-                  {...register('name')}
-                  label={t('training.responses.responseName')}
-                  hideLabel
+                <FormInput {...register('name')} label={t('training.responses.responseName')} hideLabel />
+                <Controller
+                  name="text"
+                  control={control}
+                  render={({ field }) => (
+                    <FormTextarea
+                      {...field}
+                      label={t('training.responses.responseText')}
+                      placeholder={t('training.responses.newResponseTextPlaceholder') || ''}
+                      hideLabel
+                      minRows={1}
+                      maxLength={RESPONSE_TEXT_LENGTH}
+                      showMaxLength
+                      onChange={(e) => {
+                        field.onChange(e.target.value);
+                        setEditingTrainingTitle(e.target.value);
+                      }}
+                    />
+                  )}
                 />
-                <Controller name='text' control={control} render={({ field }) => (
-                  <FormTextarea
-                    {...field}
-                    label={t('training.responses.responseText')}
-                    placeholder={t('training.responses.newResponseTextPlaceholder') || ''}
-                    hideLabel
-                    minRows={1}
-                    maxLength={RESPONSE_TEXT_LENGTH}
-                    showMaxLength
-                    onChange={(e) => {
-                      field.onChange(e.target.value)
-                      setEditingTrainingTitle(e.target.value)
-                    }}
-                  />
-                )} />
               </Track>
             </div>
             <Track gap={16}>
-              <Button appearance='secondary' onClick={() => setAddFormVisible(false)}>{t('global.cancel')}</Button>
+              <Button appearance="secondary" onClick={() => setAddFormVisible(false)}>
+                {t('global.cancel')}
+              </Button>
               <Button onClick={handleNewResponseSubmit}>{t('global.save')}</Button>
             </Track>
           </Track>
@@ -281,8 +314,8 @@ const Responses: FC = () => {
           data={formattedResponses}
           columns={responsesColumns}
           sortable
-          globalFilter={filter}
-          setGlobalFilter={setFilter}
+          isFetching={isFetching}
+          fetchNextPage={fetchNextPage}
         />
       </Card>
 
@@ -293,11 +326,10 @@ const Responses: FC = () => {
           onClose={() => setDeletableRow(null)}
           footer={
             <>
-              <Button appearance='secondary' onClick={() => setDeletableRow(null)}>{t('global.no')}</Button>
-              <Button
-                appearance='error'
-                onClick={() => responseDeleteMutation.mutate({ response: deletableRow })}
-              >
+              <Button appearance="secondary" onClick={() => setDeletableRow(null)}>
+                {t('global.no')}
+              </Button>
+              <Button appearance="error" onClick={() => responseDeleteMutation.mutate({ response: deletableRow })}>
                 {t('global.yes')}
               </Button>
             </>
@@ -308,9 +340,9 @@ const Responses: FC = () => {
       )}
 
       {refreshing && (
-          <LoadingDialog title={t('global.updatingDataHead')} >
-            <p>{t('global.updatingDataBody')}</p>
-          </LoadingDialog>
+        <LoadingDialog title={t('global.updatingDataHead')}>
+          <p>{t('global.updatingDataBody')}</p>
+        </LoadingDialog>
       )}
     </>
   );
@@ -318,24 +350,26 @@ const Responses: FC = () => {
 
 const buildDependenciesCell = (rules: JSX.Element[] | undefined, stories: JSX.Element[] | undefined) => {
   return (
-    <Label tooltip={
-      <>
-        <strong>{i18n.t('global.dependencies')}</strong>
-        {rules}
-        {stories}
-      </>
-    }>
+    <Label
+      tooltip={
+        <>
+          <strong>{i18n.t('global.dependencies')}</strong>
+          {rules}
+          {stories}
+        </>
+      }
+    >
       {i18n.t('global.dependencies')}
     </Label>
-  )
-}
+  );
+};
 
 const buildTextCell = (value: string, isEditable: boolean, onEdit: (value: string) => void) => {
-  if(isEditable) {
+  if (isEditable) {
     return (
       <FormTextarea
-        label='label'
-        name='name'
+        label="label"
+        name="name"
         defaultValue={value}
         hideLabel
         minRows={1}
@@ -343,47 +377,38 @@ const buildTextCell = (value: string, isEditable: boolean, onEdit: (value: strin
         showMaxLength
         onChange={(e) => onEdit(e.target.value)}
       />
-    )
+    );
   }
-  
-  return (
-    <p>{value}</p> 
-  )
-}
 
-const buildSaveCell = (
-  isEditable: boolean,
-  onSave: () => void,
-  onEdit: () => void,
-) => {
-  if(isEditable){
+  return <p>{value}</p>;
+};
+
+const buildSaveCell = (isEditable: boolean, onSave: () => void, onEdit: () => void) => {
+  if (isEditable) {
     return (
-      <Button appearance='text' onClick={onSave}>
+      <Button appearance="text" onClick={onSave}>
         <Icon label={i18n.t('global.save')} icon={<MdOutlineSave color={'rgba(0,0,0,0.54)'} />} />
         {i18n.t('global.save')}
       </Button>
-    )
+    );
   }
 
   return (
-    <Button
-      appearance='text'
-      onClick={onEdit}
-    >
+    <Button appearance="text" onClick={onEdit}>
       <Icon label={i18n.t('global.edit')} icon={<MdOutlineModeEditOutline color={'rgba(0,0,0,0.54)'} />} />
       {i18n.t('global.edit')}
     </Button>
-  )
-}
+  );
+};
 
 const buildDeleteCell = (onClick: () => void) => {
   return (
-    <Button appearance='text' onClick={onClick}>
+    <Button appearance="text" onClick={onClick}>
       <Icon label={i18n.t('global.delete')} icon={<MdDeleteOutline color={'rgba(0,0,0,0.54)'} />} />
       {i18n.t('global.delete')}
     </Button>
-  )
-}
+  );
+};
 
 export default withAuthorization(Responses, [
   ROLES.ROLE_ADMINISTRATOR,
--- GUI/src/services/responses.ts
+++ GUI/src/services/responses.ts
@@ -1,25 +1,30 @@
+import { PaginationParams } from 'types/api';
 import { rasaApi } from './api';
-import {ResponseEdit, ResponseDataEdit} from "types/response"
+import { ResponseEdit, ResponseDataEdit, Responses } from 'types/response';
 
-export async function editResponse(id: string,  responseText: string, update = true) {
+export const getResponses = async ({ pageParam, pageSize, filter }: PaginationParams): Promise<Responses> => {
+  const { data } = await rasaApi.get(`/responses-list?size=${pageSize}&filter=${filter}&from=${pageParam}`);
+  return data;
+};
+
+export async function editResponse(id: string, responseText: string, update = true) {
   const responseEditData = <ResponseEdit>{};
   const responseDataEdit = <ResponseDataEdit>{};
 
   responseEditData.response_name = id;
-  responseDataEdit[id] = [{text: responseText}];
+  responseDataEdit[id] = [{ text: responseText }];
   responseEditData.response = responseDataEdit;
 
   if (update) {
-    const {data} = await rasaApi.post<{ response: string }>(`responses/update`, responseEditData);
+    const { data } = await rasaApi.post<{ response: string }>(`responses/update`, responseEditData);
     return data;
   } else {
-    const {data} = await rasaApi.post<{ response: string }>(`responses/add`, responseEditData);
+    const { data } = await rasaApi.post<{ response: string }>(`responses/add`, responseEditData);
     return data;
   }
 }
 
-
-export async function deleteResponse(response: {response: string}) {
+export async function deleteResponse(response: { response: string }) {
   const { data } = await rasaApi.post(`responses/delete`, response);
   return data;
 }
