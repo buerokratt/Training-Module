2eae53feef9f0972116ea476c7aba4460e425483
Igor Krupenja <igor.krupenja@gmail.com>
2025-01-07 14:02:56 +0200

    Add dependency queries

--- DSL/OpenSearch/deploy-opensearch.sh
+++ DSL/OpenSearch/deploy-opensearch.sh
@@ -32,6 +32,7 @@ curl -L -X POST "$URL/_scripts/rule-with-name" -H 'Content-Type: application/jso
 curl -L -X POST "$URL/_scripts/rule-with-forms" -H 'Content-Type: application/json' -H 'Cookie: customJwtCookie=test' --data-binary "@templates/rule-with-forms.json"
 curl -L -X POST "$URL/_scripts/rule-with-responses" -H 'Content-Type: application/json' -H 'Cookie: customJwtCookie=test' --data-binary "@templates/rule-with-responses.json"
 curl -L -X POST "$URL/_scripts/rule-with-slots" -H 'Content-Type: application/json' -H 'Cookie: customJwtCookie=test' --data-binary "@templates/rule-with-slots.json"
+curl -L -X POST "$URL/_scripts/rules-by-responses" -H 'Content-Type: application/json' -H 'Cookie: customJwtCookie=test' --data-binary "@templates/rules-by-responses.json"
 
 # stories
 curl -XDELETE "$URL/stories?ignore_unavailable=true" -u "$AUTH" --insecure
