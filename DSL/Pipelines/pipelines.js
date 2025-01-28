6280f1aeb26af9b9878610188e3b0167ee4c5837
Varmo <101868197+varmoh@users.noreply.github.com>
2025-01-28 08:45:22 +0200

    Update pipelines.js

--- DSL/Pipelines/pipelines.js
+++ DSL/Pipelines/pipelines.js
@@ -4,10 +4,11 @@ const app = express();
 import opensearch from "./pipelines/opensearch.js";
 
 const PORT = process.env.PORT || 3010;
+const REQUEST_SIZE_LIMIT = "100mb";
 
 app.disable('x-powered-by');
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: REQUEST_SIZE_LIMIT }));
+app.use(express.urlencoded({ limit: REQUEST_SIZE_LIMIT, extended: true }));
 
 app.use(opensearch);
 
