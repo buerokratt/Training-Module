f7e8c5e3f8549091b30d7725a400e556efa8f714
VassiliMoskaljov <vassilimoskaljov@gmail.com>
2025-02-03 10:06:42 +0200

    Updated GUI and SQL

--- DSL/Resql/users/update-user-page-preferences.sql
+++ DSL/Resql/users/update-user-page-preferences.sql
@@ -1,2 +1,2 @@
-INSERT INTO user_page_preferences (user_id, page_name, page_results,created)
-VALUES (:user_id, :page_name, :page_results,:created::timestamp with time zone);
+INSERT INTO user_page_preferences (user_id, page_name, page_results, selected_columns, created)
+VALUES (:user_id, :page_name, :page_results, :selected_columns::TEXT[], :created::timestamp with time zone);
