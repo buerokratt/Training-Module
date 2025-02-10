65fac3d84aa32a5757b124e07c0fd7a890139626
Igor Krupenja <igor.krupenja@gmail.com>
2025-02-10 13:06:53 +0200

    PoC

--- GUI/src/pages/Training/Forms/index.tsx
+++ GUI/src/pages/Training/Forms/index.tsx
@@ -2,27 +2,36 @@ import { FC, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { NavigateFunction, useNavigate } from 'react-router-dom';
 import { AxiosError } from 'axios';
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { createColumnHelper } from '@tanstack/react-table';
 import { MdDeleteOutline, MdOutlineModeEditOutline } from 'react-icons/md';
 
 import { Button, Card, DataTable, Dialog, FormInput, Icon, Track } from 'components';
-import { Form } from 'types/form';
 import { useToast } from 'hooks/useToast';
-import { deleteForm } from 'services/forms';
+import { deleteForm, getForms } from 'services/forms';
 import i18n from '../../../../i18n';
 import withAuthorization, { ROLES } from 'hoc/with-authorization';
+import { useInfinitePagination } from 'hooks/useInfinitePagination';
+import { flattenPaginatedData } from 'utils/api-utils';
+import { useDebouncedFilter } from 'hooks/useDebouncedFilter';
 
 const Forms: FC = () => {
   const { t } = useTranslation();
   const toast = useToast();
   const navigate = useNavigate();
   const queryClient = useQueryClient();
-  const [filter, setFilter] = useState('');
+  const { filter, setFilter } = useDebouncedFilter();
   const [deletableForm, setDeletableForm] = useState<string | number | null>(null);
-  const { data: forms, refetch } = useQuery<Form[]>({
-    queryKey: ['forms'],
+
+  // todo rename list to just forms
+  // todo fix deletion?
+  const { data, refetch, fetchNextPage, isFetching } = useInfinitePagination<string>({
+    queryKey: ['forms-list', filter],
+    fetchFn: getForms,
+    filter,
   });
+
+  const forms = useMemo(() => flattenPaginatedData(data), [data]);
 
   const deleteFormMutation = useMutation({
     mutationFn: ({ id }: { id: string | number }) => deleteForm(id),
@@ -46,18 +55,13 @@ const Forms: FC = () => {
   });
 
   const getErrorMessage = (error: AxiosError) => {
-    if(error.response && error.response.status === 409 && error.response) {
+    if (error.response && error.response.status === 409 && error.response) {
       return t(`${error.response}`);
     }
     return error.message;
   };
 
-  const formsColumns = useMemo(() => getColumns(
-    navigate,
-    setDeletableForm,
-  ), []);
-  
-  setTimeout(() => refetch(), 1000);
+  const formsColumns = useMemo(() => getColumns(navigate, setDeletableForm), []);
 
   if (!forms) return <>Loading...</>;
 
@@ -65,19 +69,21 @@ const Forms: FC = () => {
     <>
       <h1>{t('training.forms.title')}</h1>
 
-      <Card header={
-        <Track gap={16}>
-          <FormInput
-            label={t('global.search')}
-            name='search'
-            placeholder={t('global.search') + '...'}
-            hideLabel
-            onChange={(e) => setFilter(e.target.value)}
-          />
-          <Button onClick={() => navigate('/training/forms/new')}>{t('global.add')}</Button>
-        </Track>
-      }>
-        <DataTable data={forms} columns={formsColumns} globalFilter={filter} setGlobalFilter={setFilter} sortable />
+      <Card
+        header={
+          <Track gap={16}>
+            <FormInput
+              label={t('global.search')}
+              name="search"
+              placeholder={t('global.search') + '...'}
+              hideLabel
+              onChange={(e) => setFilter(e.target.value)}
+            />
+            <Button onClick={() => navigate('/training/forms/new')}>{t('global.add')}</Button>
+          </Track>
+        }
+      >
+        <DataTable data={forms} columns={formsColumns} isFetching={isFetching} fetchNextPage={fetchNextPage} sortable />
       </Card>
 
       {deletableForm !== null && (
@@ -86,11 +92,10 @@ const Forms: FC = () => {
           onClose={() => setDeletableForm(null)}
           footer={
             <>
-              <Button appearance='secondary' onClick={() => setDeletableForm(null)}>{t('global.no')}</Button>
-              <Button
-                appearance='error'
-                onClick={() => deleteFormMutation.mutate({ id: deletableForm })}
-              >
+              <Button appearance="secondary" onClick={() => setDeletableForm(null)}>
+                {t('global.no')}
+              </Button>
+              <Button appearance="error" onClick={() => deleteFormMutation.mutate({ id: deletableForm })}>
                 {t('global.yes')}
               </Button>
             </>
@@ -103,27 +108,18 @@ const Forms: FC = () => {
   );
 };
 
-const getColumns = (
-  navigate: NavigateFunction,
-  setDeletableForm: (id: number) => void,
-) => {
-  const columnHelper = createColumnHelper<Form>();
+const getColumns = (navigate: NavigateFunction, setDeletableForm: (id: string) => void) => {
+  const columnHelper = createColumnHelper<string>();
 
   return [
-    columnHelper.accessor('form', {
+    columnHelper.accessor((row) => row, {
       header: i18n.t('training.forms.titleOne') || '',
     }),
     columnHelper.display({
       header: '',
       cell: (props) => (
-        <Button
-          appearance='text'
-          onClick={() => navigate(`/training/forms/${props.row.original.id}`)}
-        >
-          <Icon
-            label={i18n.t('global.edit')}
-            icon={<MdOutlineModeEditOutline color={'rgba(0,0,0,0.54)'} />}
-          />
+        <Button appearance="text" onClick={() => navigate(`/training/forms/${props.row.original}`)}>
+          <Icon label={i18n.t('global.edit')} icon={<MdOutlineModeEditOutline color={'rgba(0,0,0,0.54)'} />} />
           {i18n.t('global.edit')}
         </Button>
       ),
@@ -135,11 +131,8 @@ const getColumns = (
     columnHelper.display({
       header: '',
       cell: (props) => (
-        <Button appearance='text' onClick={() => setDeletableForm(props.row.original.id)}>
-          <Icon
-            label={i18n.t('global.delete')}
-            icon={<MdDeleteOutline color={'rgba(0,0,0,0.54)'} />}
-          />
+        <Button appearance="text" onClick={() => setDeletableForm(props.row.original)}>
+          <Icon label={i18n.t('global.delete')} icon={<MdDeleteOutline color={'rgba(0,0,0,0.54)'} />} />
           {i18n.t('global.delete')}
         </Button>
       ),
@@ -148,8 +141,8 @@ const getColumns = (
         size: '1%',
       },
     }),
-  ]
-}
+  ];
+};
 
 export default withAuthorization(Forms, [
   ROLES.ROLE_ADMINISTRATOR,
