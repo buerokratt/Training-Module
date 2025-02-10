aaef1c343bfeb08aadc511c595b1ae3d7501f46c
Igor Krupenja <igor.krupenja@gmail.com>
2025-02-10 14:18:53 +0200

    Fix rule details

--- GUI/src/pages/Training/Rules/ConditionNode.tsx
+++ GUI/src/pages/Training/Rules/ConditionNode.tsx
@@ -1,11 +1,14 @@
-import { FC, useEffect } from 'react';
+import { FC, useEffect, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useQuery } from '@tanstack/react-query';
 import { Controller, useFieldArray, useForm } from 'react-hook-form';
 import { Button, FormInput, FormSelect, Icon, Track } from 'components';
-import { Form } from 'types/form';
 import { MdOutlineDelete } from 'react-icons/md';
-import {Slot} from "../../../types/slot";
+import { Slot } from '../../../types/slot';
+import { useInfinitePagination } from 'hooks/useInfinitePagination';
+import { getForms } from 'services/forms';
+import { flattenPaginatedData } from 'utils/api-utils';
+import { getSlots } from 'services/slots';
 
 type NodeDataProps = {
   data: {
@@ -18,25 +21,42 @@ type NodeDataProps = {
   };
 };
 
+type SelectOption = {
+  label: string;
+  value: string;
+};
+
 type Conditions = {
-  conditions?: ({
-    active_loop?: string;
-    slot?: string;
-    value?: string;
-  }| undefined)[];
+  conditions?: (
+    | {
+        active_loop?: SelectOption;
+        slot?: SelectOption;
+        value?: string;
+      }
+    | undefined
+  )[];
 };
 
 const ConditionNode: FC<NodeDataProps> = ({ data }) => {
   const { t } = useTranslation();
-  const { data: forms } = useQuery<Form[]>({
+
+  const { data: formsData } = useInfinitePagination<string>({
     queryKey: ['forms'],
+    fetchFn: getForms,
+    pageSize: 1000,
   });
-  const { data: slots } = useQuery<Slot[]>({
+  const forms = useMemo(() => flattenPaginatedData(formsData), [formsData]);
+
+  const { data: slotsData } = useInfinitePagination<string>({
     queryKey: ['slots'],
+    fetchFn: getSlots,
+    pageSize: 1000,
   });
+  const slots = useMemo(() => flattenPaginatedData(slotsData), [slotsData]);
+
   const { control, watch } = useForm<Conditions>({
     defaultValues: {
-      conditions: [{ active_loop: '' }, { slot: '', value: '' }],
+      conditions: [{ active_loop: { label: '', value: '' } }, { slot: { label: '', value: '' }, value: '' }],
     },
   });
   const { fields, append, remove } = useFieldArray({
@@ -50,119 +70,97 @@ const ConditionNode: FC<NodeDataProps> = ({ data }) => {
   }, [watch]);
 
   return (
-      <>
-        <p>
-          <strong>
-            {t('training.conditions')}: {data.label}
-          </strong>
-        </p>
-        {fields.map((item, index) => (
-            <>
-              {item.active_loop !== undefined ? (
-                  <Track key={item.id} style={{ width: '100%' }}>
-                    <div style={{ flex: 1 }}>
-                      <Controller
-                          name={`conditions.${index}.active_loop` as const}
-                          control={control}
-                          render={({ field }) => (
-                              <FormSelect
-                                  {...field}
-                                  onSelectionChange={(selection) => {
-                                    field.onChange(selection)
-                                  }
-                                  }
-                                  value={field.value?.label ?? null}
-                                  label="active_loop"
-                                  placeholder={t('training.forms.title') || ''}
-                                  options={
-                                      forms?.map((f) => ({
-                                        label: f.form,
-                                        value: String(f.id),
-                                      })) || []
-                                  }
-                              />
-                          )}
+    <>
+      <p>
+        <strong>
+          {t('training.conditions')}: {data.label}
+        </strong>
+      </p>
+      {fields.map((item, index) => (
+        <>
+          {item.active_loop !== undefined ? (
+            <Track key={item.id} style={{ width: '100%' }}>
+              <div style={{ flex: 1 }}>
+                <Controller
+                  name={`conditions.${index}.active_loop` as const}
+                  control={control}
+                  render={({ field }) => {
+                    console.log(field.value);
+                    return (
+                      <FormSelect
+                        {...field}
+                        onSelectionChange={(selection) => {
+                          field.onChange(selection);
+                        }}
+                        value={field.value?.label}
+                        label="active_loop"
+                        placeholder={t('training.forms.title') || ''}
+                        options={forms.map((f) => ({ label: f, value: f }))}
                       />
-                    </div>
-                    <Button appearance="icon" onClick={() => remove(index)}>
-                      <Icon icon={<MdOutlineDelete fontSize={24} />} size="medium" />
-                    </Button>
-                  </Track>
-              ) : (
-                  <Track
-                      direction="vertical"
-                      gap={4}
-                      align="left"
-                      style={{
-                        padding: 4,
-                        backgroundColor: '#B3D3C0',
-                        borderRadius: 4,
-                        width: '100%',
-                      }}
-                  >
-                    <Track key={`${index}-${item.id}`} style={{ width: '100%' }}>
-                    <div style={{ flex: 1 }}>
-                        <Controller
-                            name={`conditions.${index}.slot` as const}
-                            control={control}
-                            render={({ field }) => (
-                                <FormSelect
-                                    {...field}
-                                    onSelectionChange={(selection) => {
-                                      field.onChange(selection);
-                                    }}
-                                    value={field.value?.label ?? null}
-                                    label={t('training.slot')}
-                                    options={Array.from(new Set(slots || [])).map((f) => ({
-                                      label: f.id,
-                                      value: String(f.id),
-                                    }))}
-                                />
-                            )}
-                        />
-                      </div>
-                      <Button appearance="icon" onClick={() => remove(index)}>
-                        <Icon
-                            icon={<MdOutlineDelete fontSize={24} />}
-                            size="medium"
-                        />
-                      </Button>
-                    </Track>
-                    <Track key={`${index}-${item.id}-value`} style={{ width: '90%' }}>
-                    <div style={{ flex: 1 }}>
-                        <Controller
-                            name={`conditions.${index}.value` as const}
-                            control={control}
-                            render={({ field }) => (
-                                <FormInput
-                                    {...field}
-                                    label={t('training.value')}
-                                />
-                            )}
-                        />
-                      </div>
-                    </Track>
-                  </Track>
-              )}
-            </>
-        ))}
-        <Track gap={8}>
-          <Button
-              appearance="success"
-              size="s"
-              onClick={() => append({ active_loop: '' })}
-          >
-            {t('global.add')} active_loop
-          </Button>
-          <Button
-              appearance="success"
-              size="s"
-              onClick={() => append({ slot: '', value: '' })}
-          >
-            {t('global.add')} slot
-          </Button>
-        </Track>
-      </>
+                    );
+                  }}
+                />
+              </div>
+              <Button appearance="icon" onClick={() => remove(index)}>
+                <Icon icon={<MdOutlineDelete fontSize={24} />} size="medium" />
+              </Button>
+            </Track>
+          ) : (
+            <Track
+              direction="vertical"
+              gap={4}
+              align="left"
+              style={{
+                padding: 4,
+                backgroundColor: '#B3D3C0',
+                borderRadius: 4,
+                width: '100%',
+              }}
+            >
+              <Track key={`${index}-${item.id}`} style={{ width: '100%' }}>
+                <div style={{ flex: 1 }}>
+                  <Controller
+                    name={`conditions.${index}.slot` as const}
+                    control={control}
+                    render={({ field }) => (
+                      <FormSelect
+                        {...field}
+                        onSelectionChange={(selection) => {
+                          field.onChange(selection);
+                        }}
+                        value={field.value?.label}
+                        label={t('training.slot')}
+                        options={slots.map((f) => ({ label: f, value: f }))}
+                      />
+                    )}
+                  />
+                </div>
+                <Button appearance="icon" onClick={() => remove(index)}>
+                  <Icon icon={<MdOutlineDelete fontSize={24} />} size="medium" />
+                </Button>
+              </Track>
+              <Track key={`${index}-${item.id}-value`} style={{ width: '90%' }}>
+                <div style={{ flex: 1 }}>
+                  <Controller
+                    name={`conditions.${index}.value` as const}
+                    control={control}
+                    render={({ field }) => <FormInput {...field} label={t('training.value')} />}
+                  />
+                </div>
+              </Track>
+            </Track>
+          )}
+        </>
+      ))}
+      <Track gap={8}>
+        <Button appearance="success" size="s" onClick={() => append({ active_loop: { label: '', value: '' } })}>
+          {t('global.add')} active_loop
+        </Button>
+        <Button appearance="success" size="s" onClick={() => append({ slot: { label: '', value: '' } })}>
+          {t('global.add')} slot
+        </Button>
+      </Track>
+    </>
   );
 };
 
