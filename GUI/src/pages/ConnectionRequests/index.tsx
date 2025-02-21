5ec4e18d95d4e4f879a94b6440ae21cd1a6296ab
Igor Krupenja <igor.krupenja@gmail.com>
2025-02-21 14:22:35 +0200

    Format file

--- GUI/src/pages/ConnectionRequests/index.tsx
+++ GUI/src/pages/ConnectionRequests/index.tsx
@@ -1,86 +1,92 @@
-import React, { useEffect, useMemo, useState } from "react";
-import { useTranslation } from "react-i18next";
-import { PaginationState, SortingState, createColumnHelper } from "@tanstack/react-table";
-import { format } from "date-fns";
-import { AiFillCheckCircle, AiFillCloseCircle } from "react-icons/ai";
-import { Button, Card, DataTable, Icon } from "components";
-import { Trigger } from "types/trigger";
-import { useMutation } from "@tanstack/react-query";
-import { useToast } from "hooks/useToast";
-import { updateConnectionRequest } from "services/requests";
-import withAuthorization, { ROLES } from "hoc/with-authorization";
-import { rasaApi } from "services/api";
-import { useSearchParams } from "react-router-dom";
+import React, { useEffect, useMemo, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { PaginationState, SortingState, createColumnHelper } from '@tanstack/react-table';
+import { format } from 'date-fns';
+import { AiFillCheckCircle, AiFillCloseCircle } from 'react-icons/ai';
+import { Button, Card, DataTable, Icon } from 'components';
+import { Trigger } from 'types/trigger';
+import { useMutation } from '@tanstack/react-query';
+import { useToast } from 'hooks/useToast';
+import { updateConnectionRequest } from 'services/requests';
+import withAuthorization, { ROLES } from 'hoc/with-authorization';
+import { rasaApi } from 'services/api';
+import { useSearchParams } from 'react-router-dom';
 
 const ConnectionRequests: React.FC = () => {
   const { t } = useTranslation();
   const toast = useToast();
   const [searchParams] = useSearchParams();
   const [pagination, setPagination] = useState<PaginationState>({
-    pageIndex: searchParams.get("page") ? parseInt(searchParams.get("page") as string) - 1 : 0,
+    pageIndex: searchParams.get('page') ? parseInt(searchParams.get('page') as string) - 1 : 0,
     pageSize: 10,
   });
   const [sorting, setSorting] = useState<SortingState>([]);
   const [triggers, setTriggers] = useState<Trigger[]>([]);
 
   const getTriggers = async () => {
-    let sort = "requestedAt desc";
-    if (sorting.length > 0) 
-      sort = sorting[0].id + " " + (sorting[0].desc ? "desc" : "asc");
+    let sort = 'requestedAt desc';
+    if (sorting.length > 0) sort = sorting[0].id + ' ' + (sorting[0].desc ? 'desc' : 'asc');
 
-    return rasaApi.post('/services/connection-requests', {
-      page: pagination.pageIndex + 1,
-      page_size: pagination.pageSize,
-      sorting: sort,
-    })
-    .then(res => setTriggers(res.data.response))
-    .catch(error => {
-      toast.open({
-        type: "error",
-        title: t("connectionRequests.toast.failed.requests"),
-        message: "",
+    console.log('REQUEST');
+
+    return rasaApi
+      .post('/services/connection-requests', {
+        page: pagination.pageIndex + 1,
+        page_size: pagination.pageSize,
+        sorting: sort,
+      })
+      .then((res) => setTriggers(res.data.response))
+      .catch((error) => {
+        toast.open({
+          type: 'error',
+          title: t('connectionRequests.toast.failed.requests'),
+          message: '',
+        });
+        console.error(error);
       });
-      console.error(error);
-    });
-  }
+  };
 
   useEffect(() => {
+    console.log('USE EFFECT');
     getTriggers();
   }, [pagination, sorting]);
 
   const updateRequestStatus = useMutation({
-    mutationFn: (data: { request: Trigger, status: 'approved' | 'declined' }) => 
+    mutationFn: (data: { request: Trigger; status: 'approved' | 'declined' }) =>
       updateConnectionRequest(data.request, data.status),
     onSuccess: async (data) => {
       toast.open({
-        type: "success",
-        title: data.status === "approved" 
-          ? t("connectionRequests.approvedConnection") 
-          : t("connectionRequests.declinedConnection"),
-        message: "",
+        type: 'success',
+        title:
+          data.status === 'approved'
+            ? t('connectionRequests.approvedConnection')
+            : t('connectionRequests.declinedConnection'),
+        message: '',
       });
 
       await getTriggers();
     },
     onError: () => {
       toast.open({
-        type: "error",
-        title: t("connectionRequests.toast.failed.requests"),
-        message: "",
+        type: 'error',
+        title: t('connectionRequests.toast.failed.requests'),
+        message: '',
       });
-    }
-  })
+    },
+  });
 
   const appRequestColumns = useMemo(
-    () => getColumns(
-      (trigger) => updateRequestStatus.mutate({ request: trigger, status: 'approved' }),
-      (trigger) => updateRequestStatus.mutate({ request: trigger, status: 'declined' })),
+    () =>
+      getColumns(
+        (trigger) => updateRequestStatus.mutate({ request: trigger, status: 'approved' }),
+        (trigger) => updateRequestStatus.mutate({ request: trigger, status: 'declined' })
+      ),
     []
   );
 
   return (
     <>
-      <h1>{t("connectionRequests.title")}</h1>
+      <h1>{t('connectionRequests.title')}</h1>
       <Card>
         <DataTable
           data={triggers}
@@ -89,8 +95,7 @@ const ConnectionRequests: React.FC = () => {
           sorting={sorting}
           pagination={pagination}
           setPagination={(state: PaginationState) => {
-            if (state.pageIndex === pagination.pageIndex && state.pageSize === pagination.pageSize)
-              return;
+            if (state.pageIndex === pagination.pageIndex && state.pageSize === pagination.pageSize) return;
             setPagination(state);
           }}
           setSorting={setSorting}
@@ -102,29 +107,26 @@ const ConnectionRequests: React.FC = () => {
   );
 };
 
-const getColumns = (
-  onApprove: (trigger: Trigger) => void,
-  onDecline: (trigger: Trigger) => void,
-) => {
+const getColumns = (onApprove: (trigger: Trigger) => void, onDecline: (trigger: Trigger) => void) => {
   const appRequestColumnHelper = createColumnHelper<Trigger>();
 
   return [
-    appRequestColumnHelper.accessor("intent", {
-      header: "Intent",
+    appRequestColumnHelper.accessor('intent', {
+      header: 'Intent',
       cell: (uniqueIdentifier) => uniqueIdentifier.getValue(),
     }),
-    appRequestColumnHelper.accessor("serviceName", {
-      header: "Service",
+    appRequestColumnHelper.accessor('serviceName', {
+      header: 'Service',
       cell: (uniqueIdentifier) => uniqueIdentifier.getValue(),
     }),
-    appRequestColumnHelper.accessor("requestedAt", {
-      header: "Requested At",
-      cell: (props) => <span>{format(new Date(props.getValue()), "dd-MM-yyyy HH:mm:ss")}</span>,
+    appRequestColumnHelper.accessor('requestedAt', {
+      header: 'Requested At',
+      cell: (props) => <span>{format(new Date(props.getValue()), 'dd-MM-yyyy HH:mm:ss')}</span>,
     }),
     appRequestColumnHelper.display({
-      header: "",
+      header: '',
       cell: (props) => (
-        <Button appearance='icon'>
+        <Button appearance="icon">
           <Icon
             icon={
               <AiFillCheckCircle
@@ -137,15 +139,15 @@ const getColumns = (
           />
         </Button>
       ),
-      id: "approve",
+      id: 'approve',
       meta: {
-        size: "1%",
+        size: '1%',
       },
     }),
     appRequestColumnHelper.display({
-      header: "",
+      header: '',
       cell: (props) => (
-        <Button appearance='icon'>
+        <Button appearance="icon">
           <Icon
             icon={
               <AiFillCloseCircle
@@ -158,20 +160,20 @@ const getColumns = (
           />
         </Button>
       ),
-      id: "reject",
+      id: 'reject',
       meta: {
-        size: "1%",
+        size: '1%',
       },
     }),
     appRequestColumnHelper.display({
-      header: "",
-      id: "space",
+      header: '',
+      id: 'space',
       meta: {
-        size: "1%",
+        size: '1%',
       },
     }),
   ];
-}
+};
 
 export default withAuthorization(ConnectionRequests, [
   ROLES.ROLE_ADMINISTRATOR,
