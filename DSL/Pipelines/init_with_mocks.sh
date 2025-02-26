5a153d25f045b87616681fa50a00f4c742009e10
KlviG <78801020+KlviG@users.noreply.github.com>
2025-02-26 15:28:14 +0200

    Update init_with_mocks.sh

--- DSL/Pipelines/init_with_mocks.sh
+++ DSL/Pipelines/init_with_mocks.sh
@@ -6,10 +6,9 @@ MOCK=../DMapper/locations
 PIPELINES=${1:-"localhost:3010"}
 
 # intents
-#for file in $MOCK/data/nlu/* ; do
-# curl -X POST $PIPELINES/put/intents/intent --form input=@$file
-#done
-curl -X POST $PIPELINES/bulk/rules/rule --form input=@$MOCK/data/nlu/common_klienditeenindajale_suunamine_nlu.yml
+for file in $MOCK/data/nlu/* ; do
+ curl -X POST $PIPELINES/put/intents/intent --form input=@$file
+done
 echo "intents done"
 # rules
 curl -X POST $PIPELINES/bulk/rules/rule --form input=@$MOCK/data/rules.yml
@@ -18,9 +17,9 @@ echo "rules done"
 curl -X POST $PIPELINES/bulk/stories/story --form input=@$MOCK/data/stories.yml
 echo "stories done"
 #regexes
-for file in $MOCK/data/regex/* ; do
- curl -X POST $PIPELINES/put/regexes/regex --form input=@$file
-done
+#for file in $MOCK/data/regex/* ; do
+# curl -X POST $PIPELINES/put/regexes/regex --form input=@$file
+#done
 echo "regex done"
 #domain
 curl -v -X POST $PIPELINES/bulk/domain --form input=@$MOCK/data/domain.yml
