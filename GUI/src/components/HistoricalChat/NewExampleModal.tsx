b9866a555e1bb88911e84d4f251f1998713cf6b6
1AhmedYasser <26207361+1AhmedYasser@users.noreply.github.com>
2025-04-15 12:33:06 +0200

    Added Searchable Select

--- GUI/src/components/HistoricalChat/NewExampleModal.tsx
+++ GUI/src/components/HistoricalChat/NewExampleModal.tsx
@@ -6,6 +6,8 @@ import { useForm, Controller, useWatch } from 'react-hook-form';
 import { Button, Dialog, FormInput, FormSelect, Switch, Track } from 'components';
 import { Message } from 'types/message';
 import { Intent } from 'types/intent';
+import Select from 'react-select';
+import './NewExampleModal.scss';
 
 type NewExampleForm = {
   example: string;
@@ -22,7 +24,8 @@ type NewExampleModalProps = {
 
 const NewExampleModal: FC<NewExampleModalProps> = ({ message, setMessage, onSubmitExample }) => {
   const { t } = useTranslation();
-  const [isNewIntent, setIsNewIntent] = useState<boolean>(false)
+  const [isNewIntent, setIsNewIntent] = useState<boolean>(false);
+  const [intentName, setIntentName] = useState<string>('');
   const [ selectedIntent, setSelectedIntent ] = useState<string>('');
   const { data: intents } = useQuery<Intent[]>({
     queryKey: ['intent-and-id'],
@@ -31,15 +34,8 @@ const NewExampleModal: FC<NewExampleModalProps> = ({ message, setMessage, onSubm
     mode: 'onChange',
   });
 
-  const watchIntent = useWatch({
-    control,
-    name: 'newIntent',
-  });
-
   const handleNewExample = handleSubmit((data) => {
-    if(!isNewIntent) {
-      data.intentName = selectedIntent;
-    }
+    data.intentName = !isNewIntent ? selectedIntent : intentName;
     onSubmitExample(data);
   });
 
@@ -49,36 +45,43 @@ const NewExampleModal: FC<NewExampleModalProps> = ({ message, setMessage, onSubm
       onClose={() => setMessage(null)}
       footer={
         <>
-          <Button appearance='secondary' onClick={() => setMessage(null)}>
+          <Button appearance="secondary" onClick={() => setMessage(null)}>
             {t('global.cancel')}
           </Button>
           <Button onClick={handleNewExample}>{t('global.save')}</Button>
         </>
       }
     >
-      <Track direction='vertical' gap={16} align='left'>
-        <FormInput {...register('example')} label={t('training.intents.example')} defaultValue={message.content || ''} />
+      <Track direction="vertical" gap={16} align="left">
+        <FormInput
+          {...register('example')}
+          label={t('training.intents.example')}
+          defaultValue={message.content || ''}
+        />
         {intents && !isNewIntent && (
           <Controller
-              disabled={true}
-            name='intent'
+            disabled={true}
+            name="intent"
             control={control}
             render={({ field }) => (
-              <FormSelect
-                {...field}
-                onSelectionChange={(selection) => {
-                  setSelectedIntent(selection?.value || '')
-                  field.onChange(selection)
-                }}
-                label={t('training.mba.intent')}
-                value={selectedIntent}
-                options={intents.map((intent) => ({ label: intent.intent ?? '', value: String(intent.id) }))}
-              />
+              <div className="multiSelect">
+                <label className="multiSelect__label">{t('training.mba.intent')}</label>
+                <div className="multiSelect__wrapper">
+                  <Select
+                    options={intents.map((intent) => ({ label: intent.intent ?? '', value: String(intent.id) }))}
+                    placeholder={t('global.choose')}
+                    onChange={(selected) => {
+                      setSelectedIntent(selected?.value ?? '');
+                      field.onChange(selected);
+                    }}
+                  />
+                </div>
+              </div>
             )}
           />
         )}
         <Controller
-          name='newIntent'
+          name="newIntent"
           control={control}
           render={({ field }) => (
             <Switch
@@ -87,14 +90,25 @@ const NewExampleModal: FC<NewExampleModalProps> = ({ message, setMessage, onSubm
               onLabel={t('global.yes') || ''}
               offLabel={t('global.no') || ''}
               onCheckedChange={(checked) => {
-                setIsNewIntent(checked)
-                field.onChange(checked)
+                setIsNewIntent(checked);
+                field.onChange(checked);
               }}
             />
           )}
         />
-        {watchIntent && (
-          <FormInput {...register('intentName')} label={t('training.intentName')} />
+        {isNewIntent && (
+          <FormInput
+            {...register('intentName')}
+            value={intentName}
+            label={t('training.intentName')}
+            onChange={(e) => {
+              const value = e.target.value;
+              const hasSpecialCharacters = /[^\p{L}\p{N} ]/u;
+              if (!hasSpecialCharacters.test(value) && !value.startsWith(' ')) {
+                setIntentName(value);
+              }
+            }}
+          />
         )}
       </Track>
     </Dialog>
