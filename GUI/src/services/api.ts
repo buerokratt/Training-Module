5144b5e8b66ee50e4f7838b212014e210ced37cb
1AhmedYasser <26207361+1AhmedYasser@users.noreply.github.com>
2025-04-21 10:53:38 +0200

    chore(243): Fixed Sonar Issues

--- GUI/src/services/api.ts
+++ GUI/src/services/api.ts
@@ -73,7 +73,7 @@ const AxiosInterceptor = ({ children }) => {
 
       let message = t('global.notificationErrorMsg');
 
-      if (error.response && error.response.data) {
+      if (error.response?.data) {
         const data = error.response.data;
 
         switch (true) {
@@ -87,7 +87,8 @@ const AxiosInterceptor = ({ children }) => {
             message = t('training.intents.error.intentParsing');
             break;
           case data.intentConnectedOrHasRequests:
-            message = t('training.intents.error.intentConnectedOrHasRequests');  
+            message = t('training.intents.error.intentConnectedOrHasRequests');
+            break;
           default:
             break;
         }
