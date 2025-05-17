6c6a44816c9f14d41366a3555508f9b7fe41761f
athar-mt <athar.ajmal@mindtitan.com>
2025-05-18 03:00:58 +0500

    Remove joins

--- DSL/Resql/GET/training/list_intents_last_changed.sql
+++ DSL/Resql/GET/training/list_intents_last_changed.sql
@@ -1,9 +1,9 @@
-SELECT i.id, i.intent, i.created, i.isForService
-FROM intent i
-INNER JOIN (
-    SELECT intent, MAX(created) AS max_created
+SELECT id, intent, created, isForService
+FROM intent
+WHERE (intent, created) IN (
+    SELECT intent, MAX(created)
     FROM intent
     WHERE intent IN (:intentsList) AND status = 'ACTIVE'
     GROUP BY intent
-) subquery ON i.intent = subquery.intent AND i.created = subquery.max_created
-ORDER BY i.created DESC;
+)
+ORDER BY created DESC;
