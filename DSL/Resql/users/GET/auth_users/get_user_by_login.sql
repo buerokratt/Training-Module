c6c44a2e9615b3daa106da38465bd37147c28ef2
Artsiom Beida <artsiom.beida@mindtitan.com>
2025-05-29 13:59:36 +0200

    updated get_user_by_login.sql to fetch only latest user data

--- DSL/Resql/users/GET/auth_users/get_user_by_login.sql
+++ DSL/Resql/users/GET/auth_users/get_user_by_login.sql
@@ -5,10 +5,14 @@ SELECT
     id_code,
     display_name,
     authority_name AS authorities
-FROM denormalized_user_data
+FROM denormalized_user_data d1
 WHERE
     login = :login
-  AND password_hash = :password
-  AND array_length(authority_name, 1) > 0
-ORDER BY created DESC
+    AND password_hash = :password
+    AND array_length(authority_name, 1) > 0
+    AND created = (
+        SELECT max(created)
+        FROM denormalized_user_data d2
+        WHERE d2.user_id = d1.user_id
+    )
 LIMIT 1;
