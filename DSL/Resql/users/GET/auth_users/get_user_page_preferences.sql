4bcf9fd1e2987ee86c536d3208cc52175d5c1bf9
ahmer-mt <ahmer.ali@mindtitan.com>
2025-05-29 15:47:25 +0500

    SQL-003

--- DSL/Resql/users/GET/auth_users/get_user_page_preferences.sql
+++ DSL/Resql/users/GET/auth_users/get_user_page_preferences.sql
@@ -1,4 +1,4 @@
-SELECT * FROM user_page_preferences
+SELECT id, selected_columns, created, page_results, page_name, user_id  FROM user_page_preferences
 WHERE user_id = :user_id
   AND page_name = :page_name
 ORDER BY created DESC LIMIT 1;
