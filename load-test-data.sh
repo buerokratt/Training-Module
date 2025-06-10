12be075ae4c776fa24ed22dedcc1e9f34bb4e089
Artsiom Beida <artsiom.beida@mindtitan.com>
2025-06-10 14:12:51 +0200

    unify migration flow with chatbot module

--- load-test-data.sh
+++ load-test-data.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm --network bykstack -v `pwd`/DSL/Liquibase:/liquibase/changelog liquibase/liquibase --defaultsFile=/liquibase/changelog/liquibase.properties --changelog-file=/changelog/test.yml --contexts=test update
+docker run --rm --network bykstack -v `pwd`/DSL/Liquibase/liquibase.properties:/liquibase/liquibase.properties -v `pwd`/DSL/Liquibase/test_data_changelog:/liquibase/test_data_changelog -v `pwd`/DSL/Liquibase/test.yaml:/liquibase/changelog.yaml -v `pwd`/DSL/Liquibase/data:/liquibase/data liquibase/liquibase --defaultsFile=/liquibase/liquibase.properties --contexts=test update
--- migrate.sh
+++ migrate.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm --network bykstack -v `pwd`/DSL/Liquibase:/liquibase/changelog liquibase/liquibase --defaultsFile=/liquibase/changelog/liquibase.properties update
+docker run --rm --network bykstack -v `pwd`/DSL/Liquibase/liquibase.properties:/liquibase/liquibase.properties -v `pwd`/DSL/Liquibase/changelog:/liquibase/changelog -v `pwd`/DSL/Liquibase/changelog.yaml:/liquibase/changelog.yaml -v `pwd`/DSL/Liquibase/data:/liquibase/data liquibase/liquibase --defaultsFile=/liquibase/liquibase.properties update
