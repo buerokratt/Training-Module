5981417cd2380a427becdcb8cf5d1a6f347f9775
Igor Krupenja <igor.krupenja@gmail.com>
2025-09-05 13:18:25 +0300

    fix(1021): Fix migration

--- DSL/Liquibase/changelog/20250905124512_update_intent_status_active_to_trained.sql
+++ DSL/Liquibase/changelog/20250905124512_update_intent_status_active_to_trained.sql
@@ -1,5 +1,16 @@
 -- liquibase formatted sql
 -- changeset IgorKrupenja:20250905124512
--- todo check if works
--- todo fix all occurrneces
-UPDATE intent SET status = 'NOT_TRAINED' WHERE status = 'ACTIVE';
+
+-- Create enum type for intent status
+CREATE TYPE intent_status AS ENUM ('TRAINED', 'NOT_TRAINED', 'DELETED');
+
+-- First, increase the column length to accommodate 'NOT_TRAINED' (11 characters)
+ALTER TABLE intent ALTER COLUMN status TYPE VARCHAR(15);
+
+-- Update existing data: ACTIVE -> NOT_TRAINED
+-- Also updates all other possible status values
+-- Those would actually be invalid but still possible since we were not using an enum previously
+UPDATE intent SET status = 'NOT_TRAINED' WHERE status != 'DELETED';
+
+-- Change column type to use the new enum
+ALTER TABLE intent ALTER COLUMN status TYPE intent_status USING status::intent_status;
